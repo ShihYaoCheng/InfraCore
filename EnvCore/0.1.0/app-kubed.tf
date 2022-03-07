a9fb6c988d7b77b1050e5fc06772b6057636fbc7
Costa Chen <developerjoy@gmail.com>
2022-03-07 15:11:56 +0800

    Install kubed.

--- EnvCore/0.1.0/app-kubed.tf
+++ EnvCore/0.1.0/app-kubed.tf
@@ -1,18 +1,18 @@
-﻿## https://artifacthub.io/packages/helm/appscode/kubed
-#resource "helm_release" "kubed" {
-#  name             = "kubed"
-#  repository       = "https://charts.appscode.com/stable/"
-#  chart            = "kubed"
-#  version          = "~>0.12.0"
-#  namespace        = "kube-system"
-#
-#  set {
-#    name  = "config.clusterName"
-#    value = local.GKEName
-#  }
-#
-#  set {
-#    name  = "config.configSourceNamespace"
-#    value = "cert-manager"
-#  }
-#}
+﻿# https://artifacthub.io/packages/helm/appscode/kubed
+resource "helm_release" "kubed" {
+  name             = "kubed"
+  repository       = "https://charts.appscode.com/stable/"
+  chart            = "kubed"
+  version          = "~>0.13.2"
+  namespace        = "kube-system"
+
+  set {
+    name  = "config.clusterName"
+    value = local.GKEName
+  }
+
+  set {
+    name  = "config.configSourceNamespace"
+    value = "cert-manager"
+  }
+}
