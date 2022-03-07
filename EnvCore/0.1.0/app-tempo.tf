b5741c50436587f33f561e4363b06310c48418d7
Costa Chen <developerjoy@gmail.com>
2022-03-07 16:22:22 +0800

    Install tempo.

--- EnvCore/0.1.0/app-tempo.tf
+++ EnvCore/0.1.0/app-tempo.tf
@@ -1,24 +1,22 @@
-﻿## https://artifacthub.io/packages/helm/grafana/tempo
-## https://artifacthub.io/packages/helm/grafana/tempo-distributed
-## https://github.com/grafana/helm-charts/blob/main/charts/tempo/values.yaml
-## helm repo add grafana https://grafana.github.io/helm-charts
-## helm upgrade --install tempo grafana/tempo -n tempo --create-namespace
-## helm uninstall tempo -n tempo
-#resource "helm_release" "tempo" {
-#  depends_on = [module.gke, helm_release.prometheus]
-#
-##  count = 0
-#
-#  name             = "tempo"
-#  repository       = "https://grafana.github.io/helm-charts"
-#  chart            = "tempo"
-#  version          = "~>0.14.0"
-#  namespace        = "tempo"
-#  create_namespace = true
-#
-#  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
-#    values = [
-#      templatefile("${path.module}/values/tempo.yaml", {})
-#    ]
-#}
-#
+﻿# https://artifacthub.io/packages/helm/grafana/tempo
+# https://artifacthub.io/packages/helm/grafana/tempo-distributed
+# https://github.com/grafana/helm-charts/blob/main/charts/tempo/values.yaml
+# helm repo add grafana https://grafana.github.io/helm-charts
+# helm upgrade --install tempo grafana/tempo -n tempo --create-namespace
+# helm uninstall tempo -n tempo
+resource "helm_release" "tempo" {
+  depends_on = [module.gke, helm_release.prometheus]
+
+  name             = "tempo"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "tempo"
+  version          = "~>0.14.2"
+  namespace        = "tempo"
+  create_namespace = true
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+    values = [
+      templatefile("${path.module}/values/tempo.yaml", {})
+    ]
+}
+
