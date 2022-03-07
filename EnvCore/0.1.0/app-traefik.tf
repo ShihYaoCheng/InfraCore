91fc70858a311367604ce46cd013b9b891f59f1d
Costa Chen <developerjoy@gmail.com>
2022-03-07 15:06:08 +0800

    Install Prometheus and Traefik.

--- EnvCore/0.1.0/app-traefik.tf
+++ EnvCore/0.1.0/app-traefik.tf
@@ -1,27 +1,27 @@
-﻿## https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
-## https://artifacthub.io/packages/helm/traefik/traefik
-## helm upgrade --install traefik traefik/traefik -n traefik --create-namespace
-#resource "helm_release" "traefik" {
-#  name             = "traefik"
-#  repository       = "https://helm.traefik.io/traefik"
-#  chart            = "traefik"
-#  version          = "~>10.9.1"
-#  create_namespace = true
-#  namespace        = "traefik"
-#
-#  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
-#  values = [
-#    templatefile("${path.module}/values/traefik.yaml",
-#    {
-#      env-domain-name = var.AutoRegisterDomainName ? local.EnvDomainName : ""
-#    })
-#  ]
-#}
-#
-#resource "helm_release" "traefik-resources" {
-#  depends_on = [helm_release.traefik, helm_release.prometheus]
-#
-#  name             = "traefik-resources"
-#  chart            = "../charts/traefik-resources"
-#  namespace        = "traefik"
-#}
+﻿# https://artifacthub.io/packages/helm/traefik/traefik
+# https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
+# helm upgrade --install traefik traefik/traefik -n traefik --create-namespace
+resource "helm_release" "traefik" {
+  name             = "traefik"
+  repository       = "https://helm.traefik.io/traefik"
+  chart            = "traefik"
+  version          = "~>10.14.2"
+  create_namespace = true
+  namespace        = "traefik"
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+  values = [
+    templatefile("${path.module}/values/traefik.yaml",
+    {
+      env-domain-name = var.AutoRegisterDomainName ? local.EnvDomainName : ""
+    })
+  ]
+}
+
+resource "helm_release" "traefik-resources" {
+  depends_on = [helm_release.traefik, helm_release.prometheus]
+
+  name             = "traefik-resources"
+  chart            = "../charts/traefik-resources"
+  namespace        = "traefik"
+}
