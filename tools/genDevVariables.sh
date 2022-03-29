da913beebd9070c2cca1d076e1d01bcd192679a1
Costa Chen <developerjoy@gmail.com>
2022-03-29 09:29:09 +0800

    Fix path.

--- tools/genDevVariables.sh
+++ tools/genDevVariables.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # execute dos2unix [filename] if [No such file or directory] message appear.
 
-base64 -w 0 ../env-dev/terraform.tfvars > devVariables.txt
+base64 -w 0 ../EnvDev/terraform.tfvars > devVariables.txt
