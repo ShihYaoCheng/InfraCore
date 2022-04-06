5cd5d6da3a4bb90bb4fa93aaa6031fad0b39d9d0
Costa Chen <developerjoy@gmail.com>
2022-04-06 14:05:15 +0800

    Add OpApps root module for SRE environment.

---
+++ EnvSRE/3-OpApps/main.tf
@@ -0,0 +1,12 @@
+﻿# https://polaris.docs.fairwinds.com/
+# https://github.com/FairwindsOps/polaris
+# https://github.com/FairwindsOps/charts/tree/master/stable/polaris
+# Best Practices for Kubernetes Workload Configuration.
+resource "helm_release" "polaris" {
+  name             = "polaris"
+  repository       = "https://charts.fairwinds.com/stable"
+  chart            = "polaris"
+  version          = "~>5.1.0"
+  create_namespace = true
+  namespace        = "fairwinds"
+}
---
+++ EnvSRE/3-OpApps/providers.tf
@@ -0,0 +1,36 @@
+﻿terraform {
+  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
+  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
+  # https://www.terraform.io/language/expressions/version-constraints
+  required_version = "~> 1.0.11"
+
+  required_providers {
+    // https://registry.terraform.io/providers/hashicorp/helm/latest
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.5.0"
+    }
+  }
+}
+
+data "google_client_config" "default" {}
+
+data "google_storage_bucket_object_content" "api" {
+  name   = "api"
+  bucket = "sk-sre"
+}
+
+data "google_storage_bucket_object_content" "ca" {
+  bucket = "sk-sre"
+  name   = "ca"
+}
+
+provider "helm" {
+  debug = true
+
+  kubernetes {
+    host                   = "https://${data.google_storage_bucket_object_content.api.content}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.ca.content)
+  }
+}
