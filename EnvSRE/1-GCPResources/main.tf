42bce06c32989c4fc2fda5fe536b98cac7f09054
Costa Chen <developerjoy@gmail.com>
2022-04-06 16:29:26 +0800

    Add GCPResource module.

---
+++ EnvSRE/1-GCPResources/main.tf
@@ -0,0 +1,7 @@
+﻿module "sk-sre-bucket" {
+  source  = "../../Modules/1-GCPResources/0.1.0"
+
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCSBucketName = "sk-sre"
+}
---
+++ Modules/1-GCPResources/0.1.0/main.tf
@@ -0,0 +1,19 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/cloud-storage/google/latest/submodules/simple_bucket
+module "sk-bucket" {
+  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version = "~>1.3"
+
+  project_id = var.GCPProjectID
+  # https://cloud.google.com/storage/docs/locations#available-locations
+  location   = var.GCPRegion
+
+  name = var.GCSBucketName
+
+  # https://cloud.google.com/storage/docs/storage-classes#available_storage_classes
+  storage_class = "NEARLINE"
+
+  # When deleting a bucket, this boolean option will delete all contained objects.
+  # If false, Terraform will fail to delete buckets which contain objects.
+  force_destroy = true
+}
+
