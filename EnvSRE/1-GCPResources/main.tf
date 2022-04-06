e219aa620fec489d5eafab44e1ddf5a249582806
Costa Chen <developerjoy@gmail.com>
2022-04-06 16:41:31 +0800

    rename.

--- EnvSRE/1-GCPResources/main.tf
+++ EnvSRE/1-GCPResources/main.tf
@@ -1,5 +1,5 @@
 ﻿module "sk-sre-bucket" {
-  source  = "../../Modules/1-GCPResources/0.1.0"
+  source  = "../../Modules/GCPResources/0.1.0"
 
   GCPProjectID = var.GCPProjectID
   GCPRegion = var.GCPRegion
