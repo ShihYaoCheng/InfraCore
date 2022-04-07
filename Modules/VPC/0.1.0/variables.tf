ed90cea93e9878dd18dcb5f22aecd44c3fed4167
Costa Chen <developerjoy@gmail.com>
2022-04-07 11:05:38 +0800

    Add VPC module.

---
+++ Modules/VPC/0.1.0/variables.tf
@@ -0,0 +1,19 @@
+﻿#============================
+# Project
+#============================
+variable "GCPProjectID" {
+  type    = string
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+}
+
+variable "VPCName" {
+  type = string
+}
+
+variable "GCSBucketName" {
+  type = string
+}
