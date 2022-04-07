7114aafcadb18f90dc85754da21fa406ed8e5d1d
Costa Chen <developerjoy@gmail.com>
2022-04-07 15:15:18 +0800

    Create apps with service account.

--- EnvSRE/4-OpApps/providers.tf
+++ EnvSRE/4-OpApps/providers.tf
@@ -5,12 +5,26 @@
   required_version = "~> 1.0.11"
 
   required_providers {
+    // https://registry.terraform.io/providers/hashicorp/google/latest
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.16.0"
+    }
+
     // https://registry.terraform.io/providers/hashicorp/helm/latest
     helm = {
       source  = "hashicorp/helm"
       version = "~>2.5.0"
     }
   }
+}
+
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = var.GCPProjectID // assign default value.
+  region  = var.GCPRegion    // assign default value.
+  zone    = var.GCPZone      // assign default value.
+  credentials = file("../../../keys/gitlab-sk-infra-op.json")
 }
 
 data "google_client_config" "default" {}
--- EnvSRE/4-OpApps/variables.tf
+++ EnvSRE/4-OpApps/variables.tf
@@ -2,6 +2,26 @@
   GCSBucketName = "cqi-sk-sre"
 }
 
+//============================== Project ==============================
+variable "GCPProjectID" {
+  type    = string
+  default = "stellar-38931"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "asia-east1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "asia-east1-b"
+  description = "cloud provider zone."
+}
+
+
 //============================== External-DNS ==============================
 variable "GodaddyAPIKey" {
   type      = string
