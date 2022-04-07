6570ed4d9bf703ee8d00fd2eebf4f9289add1b70
Costa Chen <developerjoy@gmail.com>
2022-04-07 14:46:55 +0800

    test

--- EnvDev/4-OpApps/providers.tf
+++ EnvDev/4-OpApps/providers.tf
@@ -7,12 +7,25 @@
   backend "http" {}
 
   required_providers {
+    // https://registry.terraform.io/providers/hashicorp/google/latest
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.16.0"
+    }
+
     // https://registry.terraform.io/providers/hashicorp/helm/latest
     helm = {
       source  = "hashicorp/helm"
       version = "~>2.5.0"
     }
   }
+}
+
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = var.GCPProjectID // assign default value.
+  region  = var.GCPRegion    // assign default value.
+  zone    = var.GCPZone      // assign default value.
 }
 
 data "google_client_config" "default" {}
