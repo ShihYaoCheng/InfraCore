447905853ae00f1cd28aa09fab58facae59671f7
Costa Chen <developerjoy@gmail.com>
2022-04-07 13:08:22 +0800

    Add GKE root module.

---
+++ EnvDev/3-GKE/main.tf
@@ -0,0 +1,12 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "gke" {
+  source  = "../../Modules/GKE/0.1.0"
+
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCPZone = var.GCPZone
+
+  GKEName = "cqi-sk-dev"
+  GCSBucketName = "cqi-sk-dev"
+}
+
