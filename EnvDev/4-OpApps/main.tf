17ed3af6e02b88a709d9e385ab7aa468c8ce1a9d
Costa Chen <developerjoy@gmail.com>
2022-04-08 15:04:08 +0800

    fix bug.

--- EnvDev/4-OpApps/main.tf
+++ EnvDev/4-OpApps/main.tf
@@ -2,7 +2,7 @@
 module "OpApps" {
   source  = "../../Modules/OpApps/0.1.0"
 
-  ProjectName = "cqi-sk-dev"
+  ProjectName = var.ProjectName
   DomainName = "dev.origingaia.com"
 
   AlertSlackChannel          = "alert-sk-dev"
