2c8b7f6895089b7139db83543ea377e36e6172d7
Costa Chen <developerjoy@gmail.com>
2022-04-13 11:48:34 +0800

    Add OpApps.

---
+++ EnvQA/4-OpApps/main.tf
@@ -0,0 +1,23 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "OpApps" {
+  source = "../../Modules/OpApps/0.1.0"
+
+  ProjectName = var.ProjectName
+  DomainName  = "qa.origingaia.com"
+
+  ArgoCD_GitLabTokenName              = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_RepositoryHelmPathValueFiles = "{values-prod.yaml}"
+  ArgoCD_AppFileBranchOrTag           = "v0.0.8f1"
+  ArgoCD_AppTableBranchOrTag          = "v0.0.2f1"
+  ArgoCD_AppUserBranchOrTag           = "v0.0.3f1"
+  ArgoCD_AppBattleBranchOrTag         = "v0.0.2f1"
+
+  AlertSlackChannel          = "alert-sk-qa"
+  PrometheusStorageClassName = "ssd-delete"
+  PrometheusStorageSize      = "20Gi"
+  GrafanaAdminPassword       = "ran9977"
+}
+
+
+
---
+++ EnvQA/4-OpApps/variables.tf
@@ -0,0 +1,40 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+  default = "cqi-sk-qa"
+}
+
+variable "GCPProjectID" {
+  type    = string
+  default = "cqi-operation"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "asia-east1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "asia-east1-b"
+  description = "cloud provider zone."
+}
+
+#============================
+# ArgoCD                    #
+#============================
+variable "ArgoCD_GitLabTokenName" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+variable "ArgoCD_GitLabTokenSecret" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
