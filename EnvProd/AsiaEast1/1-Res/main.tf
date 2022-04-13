23433954b710743323e78d6f0d97d7f03837fd08
Costa Chen <developerjoy@gmail.com>
2022-04-13 13:21:02 +0800

    Add Production asia east1 resource module.

---
+++ EnvProd/AsiaEast1/1-Res/main.tf
@@ -0,0 +1,11 @@
+﻿module "sk-qa-res" {
+  source  = "../../../Modules/Resources/0.1.0"
+
+  # https://cloud.google.com/storage/docs/troubleshooting#bucket-name-conflict
+  # The GCS bucket name must be unique in the entire GCP. If you have a 409 conflict error
+  # which means the bucket name has been used by another user, you should use another name.
+  ProjectName = "cqi-sk-prod-asia-east1"
+
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+}
