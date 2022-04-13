e16c15197ac3e7275f9cf046cd9d54361bef338a
Costa Chen <developerjoy@gmail.com>
2022-04-13 10:57:19 +0800

    Add VPC module.

---
+++ EnvQA/2-VPC/main.tf
@@ -0,0 +1,9 @@
+# https://registry.terraform.io/modules/terraform-google-modules/network/google/latest
+module "vpc" {
+  source  = "../../Modules/VPC/0.1.0"
+
+  ProjectName = "cqi-sk-qa"
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+}
+
