a635ad5979633197227759a3319aeb578dec6203
Costa Chen <developerjoy@gmail.com>
2022-04-13 15:07:51 +0800

    Add GKE for us.

---
+++ EnvProd/USCentral1/3-GKE/main.tf
@@ -0,0 +1,13 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "gke" {
+  source  = "../../../Modules/GKE/0.1.0"
+
+  ProjectName = "cqi-sk-prod-us-central1"
+  GKE-VPCName = "cqi-sk-prod"
+  GKE-Zones = ["us-central1-c"]
+
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCPZone = var.GCPZone
+}
+
---
+++ EnvProd/USCentral1/3-GKE/variables.tf
@@ -0,0 +1,20 @@
+﻿#============================
+# Project                   #
+#============================
+variable "GCPProjectID" {
+  type    = string
+  default = "cqi-operation"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "us-central1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "us-central1-c"
+  description = "cloud provider zone."
+}
