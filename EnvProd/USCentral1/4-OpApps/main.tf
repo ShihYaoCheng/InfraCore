3745e37c9450aafd959ddcdd4688d30c543448c3
Costa Chen <developerjoy@gmail.com>
2022-04-13 16:14:58 +0800

    Add US module.

---
+++ EnvProd/USCentral1/4-OpApps/main.tf
@@ -0,0 +1,23 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "OpApps" {
+  source = "../../../Modules/OpApps/0.1.0"
+
+  ProjectName = var.ProjectName
+  DomainName  = "us.origingaia.com"
+
+  ArgoCD_GitLabTokenName              = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_RepositoryHelmPathValueFiles = "{values-prod.yaml}"
+  ArgoCD_AppFileBranchOrTag           = "v0.0.9"
+  ArgoCD_AppTableBranchOrTag          = "v0.0.3"
+  ArgoCD_AppUserBranchOrTag           = "v0.0.4"
+  ArgoCD_AppBattleBranchOrTag         = "v0.0.2f2"
+
+  AlertSlackChannel          = "alert-sk-prod-us"
+  PrometheusStorageClassName = "ssd-retain"
+  PrometheusStorageSize      = "500Gi"
+  GrafanaAdminPassword       = var.GrafanaAdminPassword
+}
+
+
+
---
+++ EnvProd/USCentral1/4-OpApps/variables.tf
@@ -0,0 +1,48 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+  default = "cqi-sk-prod-us-central1"
+}
+
+variable "GCPProjectID" {
+  type    = string
+  default = "cqi-operation"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "us-central1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "us-central1-c"
+  description = "cloud provider zone."
+}
+
+#============================
+# ArgoCD                    #
+#============================
+variable "ArgoCD_GitLabTokenName" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+variable "ArgoCD_GitLabTokenSecret" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+#============================
+# Grafana                   #
+#============================
+variable "GrafanaAdminPassword" {
+  type      = string
+  sensitive = true
+}
