d0c5391aa7ab858fdd097e6a0fd172fe95f666e0
Costa Chen <developerjoy@gmail.com>
2022-04-13 15:00:20 +0800

    Add GKE for eu.

---
+++ EnvProd/EuropeWest2/3-GKE/main.tf
@@ -0,0 +1,13 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "gke" {
+  source  = "../../../Modules/GKE/0.1.0"
+
+  ProjectName = "cqi-sk-prod-eu-west2"
+  GKE-VPCName = "cqi-sk-prod"
+  GKE-Zones = ["europe-west2-b"]
+
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCPZone = var.GCPZone
+}
+
