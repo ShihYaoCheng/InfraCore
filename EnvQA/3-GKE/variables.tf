101eee01ac763ff136f2ac0b6abb703f933ccd19
Costa Chen <developerjoy@gmail.com>
2022-04-13 11:18:48 +0800

    Add GKE.

---
+++ EnvQA/3-GKE/variables.tf
@@ -0,0 +1,20 @@
+﻿#============================
+# Project                   #
+#============================
+variable "GCPProjectID" {
+  type    = string
+  default = "cqi-operation"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "asia-east1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "asia-east1-b"
+  description = "cloud provider zone."
+}
