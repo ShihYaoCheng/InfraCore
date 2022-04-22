30b629dc4f790e124ef6b2abd5c1f55cb0007974
Costa Chen <developerjoy@gmail.com>
2022-04-22 11:26:34 +0800

    add provider requirement to child moudle.

--- EnvSRE/3-GKE/providers.tf
+++ EnvSRE/3-GKE/providers.tf
@@ -1,17 +1,17 @@
-﻿terraform {
-  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
-  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
-  # https://www.terraform.io/language/expressions/version-constraints
-  required_version = "~> 1.0.11"
-
-  required_providers {
-    // https://registry.terraform.io/providers/hashicorp/google/latest
-    google = {
-      source = "hashicorp/google"
-      version = "~>4.16.0"
-    }
-  }
-}
+﻿#terraform {
+#  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
+#  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
+#  # https://www.terraform.io/language/expressions/version-constraints
+#  required_version = "~> 1.0.11"
+#
+#  required_providers {
+#    // https://registry.terraform.io/providers/hashicorp/google/latest
+#    google = {
+#      source = "hashicorp/google"
+#      version = "~>4.16.0"
+#    }
+#  }
+#}
 
 // https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
