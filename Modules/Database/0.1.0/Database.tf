35a9ce1425867f7640be49101b70a01169c0012c
Costa <developerjoy@gmail.com>
2022-05-20 14:15:13 +0800

    Fix error.

--- Modules/Database/0.1.0/Database.tf
+++ Modules/Database/0.1.0/Database.tf
@@ -1,8 +1,10 @@
 ﻿# https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/database
 resource "mysql_database" "User" {
+  depends_on = [module.CloudSQL]
   name = "User"
 }
 
 resource "mysql_database" "Backstage" {
+  depends_on = [module.CloudSQL]
   name = "Backstage"
 }
--- Modules/Database/0.1.0/Users.tf
+++ Modules/Database/0.1.0/Users.tf
@@ -2,6 +2,7 @@
 
 # https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/user
 resource "mysql_user" "cqi-backstage" {
+  depends_on = [module.CloudSQL]
   user               = "cqi-backstage"
   host               = "%" // allow any host to connected. Default: localhost(allow localhost connected only).
   plaintext_password = var.CloudSQLBackstagePassword
@@ -9,6 +10,7 @@ resource "mysql_user" "cqi-backstage" {
 
 # https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/grant
 resource "mysql_grant" "cqi-backstage" {
+  depends_on = [module.CloudSQL]
   user       = mysql_user.cqi-backstage.user
   host       = mysql_user.cqi-backstage.host
   database   = "Backstage"
@@ -17,12 +19,14 @@ resource "mysql_grant" "cqi-backstage" {
 
 
 resource "mysql_user" "cqi-user" {
+  depends_on = [module.CloudSQL]
   user               = "cqi-user"
   host               = "%" // allow any host to connected. Default: localhost(allow localhost connected only).
   plaintext_password = var.CloudSQLUserPassword
 }
 
 resource "mysql_grant" "cqi-user" {
+  depends_on = [module.CloudSQL]
   user       = mysql_user.cqi-user.user
   host       = mysql_user.cqi-user.host
   database   = "User"
