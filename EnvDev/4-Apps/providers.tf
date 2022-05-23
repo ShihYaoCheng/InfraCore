2edc8a463cad4660efe30de7ca407c95a07072fd
Costa <developerjoy@gmail.com>
2022-05-23 11:48:40 +0800

    Remove unnecessary settings.

--- EnvDev/4-Apps/providers.tf
+++ EnvDev/4-Apps/providers.tf
@@ -1,24 +1,5 @@
 ﻿terraform {
-  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
-  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
-  # https://www.terraform.io/language/expressions/version-constraints
-  required_version = "~> 1.0.11"
-
   backend "http" {}
-
-  required_providers {
-    // https://registry.terraform.io/providers/hashicorp/google/latest
-    google = {
-      source = "hashicorp/google"
-      version = "~>4.16.0"
-    }
-
-    // https://registry.terraform.io/providers/hashicorp/helm/latest
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~>2.5.0"
-    }
-  }
 }
 
 // https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
