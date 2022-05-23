50303566c19f7a915967d81b166a6f88dfc10fb3
Costa <developerjoy@gmail.com>
2022-05-23 15:59:22 +0800

    rename.

--- Modules/Apps/0.1.0/CloudSQL-WorkloadIdentity.tf
+++ Modules/Apps/0.1.0/CloudSQL-WorkloadIdentity.tf
@@ -1,15 +1,13 @@
 # https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/workload-identity
-module "kubernetes-engine_workload-identity" {
+module "backstage-workload-identity" {
   count   = 0
   source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   version = "21.0.0"
 
-  name         = "test100"
+  name         = "auth-proxy"
   project_id   = var.GCPProjectID
-  #  gcp_sa_name = "test100"
-  #  k8s_sa_name = "test100"
-  namespace    = "default"
-  roles        = ["roles/storage.admin"]
+  namespace    = "backstage"
+  roles        = ["roles/cloudsql.admin"]
   cluster_name = var.ProjectName
   location     = var.GCPZone
 }
