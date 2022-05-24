0f7ab226062275571bfc760a1362971634488c2d
Costa Chen <developerjoy@gmail.com>
2022-05-24 11:14:37 +0800

    Add Database settings for Dev environment.

---
+++ EnvDev/3-Database/main.tf
@@ -0,0 +1,19 @@
+﻿module "database" {
+  source  = "../../Modules/Database/0.1.0"
+
+  ProjectName = "cqi-sk-dev"
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCPZone = var.GCPZone
+  
+  CloudSQLAdminPassword = "admin1234"
+  CloudSQLBackstagePassword = "backstage1234"
+  CloudSQLUserPassword = "user1234"
+  CloudSQLEnableAutoBackup = false
+  CloudSQLEnablePointInTimeRecovery = false
+  CloudSQLEnableDiskAutoResize = false
+  CloudSQLEnableHighlyAvailable = false
+  CloudSQLAllowDeletion = true
+  CloudSQLMachine = "db-f1-micro"
+  CloudSQLAllowPublicIP = "118.168.34.41/32"
+}
---
+++ EnvDev/3-Database/providers.tf
@@ -0,0 +1,33 @@
+﻿# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = var.GCPProjectID // assign default value.
+  region  = var.GCPRegion    // assign default value.
+  zone    = var.GCPZone      // assign default value.
+}
+
+provider "google-beta" {
+  project = var.GCPProjectID // assign default value.
+  region  = var.GCPRegion    // assign default value.
+  zone    = var.GCPZone      // assign default value.
+}
+
+provider "mysql" {
+  endpoint = module.database.PublicIP
+  username = module.database.AdminName
+  password = "admin1234"
+}
+
+terraform {
+  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
+  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
+  # https://www.terraform.io/language/expressions/version-constraints
+  required_version = "~> 1.0.11"
+
+  required_providers {
+      # https://registry.terraform.io/providers/winebarrel/mysql/latest
+    mysql = {
+      source = "winebarrel/mysql"
+      version = "1.10.6"
+    }
+  }
+}
