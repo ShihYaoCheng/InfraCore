72346308dd3f47644eddf6e839cd6e95a800373c
Costa Chen <developerjoy@gmail.com>
2022-05-24 11:44:03 +0800

    Avoid name conflicts caused by different environments.

--- Modules/Apps/0.1.0/CloudSQL-WorkloadIdentity.tf
+++ Modules/Apps/0.1.0/CloudSQL-WorkloadIdentity.tf
@@ -3,23 +3,32 @@ module "backstage-workload-identity" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   version = "21.0.0"
 
-  name         = "backstage-cloud-sql-proxy"
   project_id   = var.GCPProjectID
-  namespace    = "backstage"
-  roles        = ["roles/cloudsql.client"]
   cluster_name = var.ProjectName
   location     = var.GCPZone
+  
+  name         = "backstage-cloud-sql-proxy"
+  gcp_sa_name = "${var.ProjectName}-backstage-sql-proxy"
+  k8s_sa_name = "cloud-sql-proxy"
+  
+  namespace    = "backstage"
+  roles        = ["roles/cloudsql.client"]
 }
 
 module "user-workload-identity" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   version = "21.0.0"
 
-  name         = "user-cloud-sql-proxy"
   project_id   = var.GCPProjectID
-  namespace    = "user"
-  roles        = ["roles/cloudsql.client"]
   cluster_name = var.ProjectName
   location     = var.GCPZone
+  
+  name         = "user-cloud-sql-proxy"
+  gcp_sa_name = "${var.ProjectName}-user-sql-proxy"
+  k8s_sa_name = "cloud-sql-proxy"
+  
+  namespace    = "user"
+  roles        = ["roles/cloudsql.client"]
+  
 }
 
