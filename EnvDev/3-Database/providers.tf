9a3298091e198a6429183a1d433b2df2a0839f9a
Costa Chen <developerjoy@gmail.com>
2022-05-24 11:55:48 +0800

    Fix error.(lack http backend, don't create tfstate file in GitLab.)

--- EnvDev/3-Database/providers.tf
+++ EnvDev/3-Database/providers.tf
@@ -18,10 +18,7 @@ provider "mysql" {
 }
 
 terraform {
-  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
-  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
-  # https://www.terraform.io/language/expressions/version-constraints
-  required_version = "~> 1.0.11"
+  backend "http" {}
 
   required_providers {
       # https://registry.terraform.io/providers/winebarrel/mysql/latest
