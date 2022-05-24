84663c068fcd1b8100ce953cfaaafbe11ecf6332
Costa Chen <developerjoy@gmail.com>
2022-05-24 11:05:47 +0800

    Remove deprecated files.

--- Deprecated/EnvCore/0.1.0/configuration.tf
+++
@@ -1,5 +0,0 @@
-﻿resource "helm_release" "configuration" {
-  name             = "configuration"
-  chart            = "../charts/sk-configuration"
-  namespace        = "default"
-}
--- Deprecated/EnvCore/0.1.0/outputs.tf
+++
@@ -1,7 +0,0 @@
-﻿output "gke-endpoints" {
-  value = module.gke.endpoint
-}
-
-output "gke-ca-certificate" {
-  value = module.gke.ca_certificate
-}
