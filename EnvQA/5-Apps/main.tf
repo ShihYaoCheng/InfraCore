5dcfa6ffab3a2f0ea3bc6e125f4c3a20cf36d8f5
Costa Chen <developerjoy@gmail.com>
2022-05-31 15:27:29 +0800

    upgrade battle.

--- EnvQA/5-Apps/main.tf
+++ EnvQA/5-Apps/main.tf
@@ -13,7 +13,7 @@ module "Apps" {
   ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
   ArgoCD_RepositoryHelmPathValueFiles = "{values-prod.yaml}"
   ArgoCD_AppBackstageBranchOrTag      = "v0.3.0f2"
-  ArgoCD_AppBattleBranchOrTag         = "v0.3.0f1"
+  ArgoCD_AppBattleBranchOrTag         = "v0.3.0f3"
   ArgoCD_AppFileBranchOrTag           = "v0.3.0f2"
   ArgoCD_AppTableBranchOrTag          = "v0.3.0f1"
   ArgoCD_AppUserBranchOrTag           = "v0.3.0f1"
