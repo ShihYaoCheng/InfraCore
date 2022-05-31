df0414adb23b5fc9a7cfd340e8884771da87d66b
Costa Chen <developerjoy@gmail.com>
2022-05-31 13:56:30 +0800

    rename.

--- tools/genQAVarBase64.sh
+++ tools/genQAVarBase64.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # execute dos2unix [filename] if [No such file or directory] message appear.
 
-base64 -w 0 ../EnvProduction/terraform.tfvars > EnvProductionVariablesBase64.txt
+base64 -w 0 ../EnvQA/5-Apps/terraform.tfvars
