8e21d3f3dd0394fae33caa3efc2da36d04d67bbd
Costa Chen <developerjoy@gmail.com>
2022-05-31 08:58:41 +0800

    Upgrade to new settings.

--- EnvQA/4-GKE/main.tf
+++ EnvQA/4-GKE/main.tf
@@ -6,5 +6,14 @@ module "gke" {
   GCPProjectID = var.GCPProjectID
   GCPRegion = var.GCPRegion
   GCPZone = var.GCPZone
+  GKE-Zones = ["asia-east1-a"]
+
+  GKE-AutoScaling-MinCPU = 1
+  GKE-AutoScaling-MaxCPU = 5
+  GKE-AutoScaling-MinMemoryGB = 5
+  GKE-AutoScaling-MaxMemoryGB = 15
+  GKE-NodeCount-e2-medium = 2
+  GKE-NodeCount-e2-high-cpu-4 = 0
+  GKE-NodeCount-e2-standard-2 = 1
 }
 
