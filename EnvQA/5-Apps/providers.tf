2e89e2347c2e5c909e2d38ef4eae0deb081b6a46
Costa Chen <developerjoy@gmail.com>
2022-05-31 13:34:40 +0800

    Fix error.

--- EnvQA/5-Apps/providers.tf
+++ EnvQA/5-Apps/providers.tf
@@ -49,3 +49,9 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA.content)
   }
 }
+
+provider "kubernetes" {
+  host = "https://${data.google_storage_bucket_object_content.GKE-API.content}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA.content)
+}
