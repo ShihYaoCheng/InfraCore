173dcec4daa5ec07aca07493d261cef86946c76c
Costa Chen <developerjoy@gmail.com>
2022-06-13 10:16:58 +0800

    The CloudRun replace with module.

--- Modules/CloudRun/0.1.0/Battle.tf
+++ Modules/CloudRun/0.1.0/Battle.tf
@@ -1,38 +1,66 @@
-﻿resource "google_cloud_run_service" "battle" {
-  name     = var.CloudRunName
+﻿# https://registry.terraform.io/modules/GoogleCloudPlatform/cloud-run/google/latest
+module "cloud-run" {
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "0.3.0"
+  
+  project_id = var.GCPProjectID
+  image = var.CloudRunImage
   location = var.GCPRegion
-
-  template {
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/minScale" = var.CloudRunMinScale
-        "autoscaling.knative.dev/maxScale" = var.CloudRunMaxScale
-#        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
-      }
-    }
-    spec {
-      containers {
-        image = var.CloudRunImage
-        ports {
-          name = "http1"
-#          protocol = "TCP"
-          container_port = 8080
-        }
-        resources {
-          limits = {
-            memory: "512Mi"
-            cpu: "1000m"
-          }
-        }
-      }
-    }
+  
+  service_name = var.CloudRunName
+  
+  template_annotations = {
+    "autoscaling.knative.dev/minScale" = var.CloudRunMinScale
+    "autoscaling.knative.dev/maxScale" = var.CloudRunMaxScale
+    #        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
+  
+  limits = {
+    memory: "512Mi"
+    cpu: "1000m"
+  }
+  
+  ports = {
+    "name": "http1",
+    "port": 8080
   }
 }
+
+#resource "google_cloud_run_service" "battle" {
+#  name     = var.CloudRunName
+#  location = var.GCPRegion
+#
+#  template {
+#    metadata {
+#      annotations = {
+#        "autoscaling.knative.dev/minScale" = var.CloudRunMinScale
+#        "autoscaling.knative.dev/maxScale" = var.CloudRunMaxScale
+##        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
+#      }
+#    }
+#    spec {
+#      containers {
+#        image = var.CloudRunImage
+#        ports {
+#          name = "http1"
+##          protocol = "TCP"
+#          container_port = 8080
+#        }
+#        resources {
+#          limits = {
+#            memory: "512Mi"
+#            cpu: "1000m"
+#          }
+#        }
+#      }
+#    }
+#  }
+#
+#  traffic {
+#    percent         = 100
+#    latest_revision = true
+#  }
+#}
 
 data "google_iam_policy" "noAuth" {
   binding {
@@ -46,7 +74,8 @@ data "google_iam_policy" "noAuth" {
 resource "google_cloud_run_service_iam_policy" "noAuth" {
   project     = var.GCPProjectID
   location    = var.GCPRegion
-  service     = google_cloud_run_service.battle.name
+#  service     = google_cloud_run_service.battle.name
+  service     = module.cloud-run.service_name
 
   policy_data = data.google_iam_policy.noAuth.policy_data
 }
