3481820e8a54b2f491e57ee4808bfeef02d756a3
Costa Chen <developerjoy@gmail.com>
2022-06-14 09:19:39 +0800

    Avoid subnet name conflict.

--- Modules/VPC/0.1.0/main.tf
+++ Modules/VPC/0.1.0/main.tf
@@ -12,21 +12,21 @@ module "VPC" {
   # https://www.calculator.net/ip-subnet-calculator.html
   subnets = [
     {
-      subnet_name   = "london"
+      subnet_name   = "${var.ProjectName}-london"
       subnet_ip     = "192.168.0.0/28"
       subnet_region = "europe-west2"
       subnet_private_access = true # Whether this subnet will have private Google access enabled
       subnet_flow_logs = false # Whether the subnet will record and send flow log data to logging
     },
     {
-      subnet_name   = "iowa"
+      subnet_name   = "${var.ProjectName}-iowa"
       subnet_ip     = "192.168.0.16/28"
       subnet_region = "us-central1"
       subnet_private_access = true # Whether this subnet will have private Google access enabled
       subnet_flow_logs = false # Whether the subnet will record and send flow log data to logging
     },
     {
-      subnet_name   = "taiwan"
+      subnet_name   = "${var.ProjectName}-taiwan"
       subnet_ip     = "192.168.0.32/28"
       subnet_region = "asia-east1"
       subnet_private_access = true # Whether this subnet will have private Google access enabled
