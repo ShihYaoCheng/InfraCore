9783a85eed7dcd976c4b5183904cdd7acf37bdf5
Costa Chen <developerjoy@gmail.com>
2022-06-14 11:47:20 +0800

    Init version.

---
+++ EnvQA/6-CloudRun/main.tf
@@ -0,0 +1,22 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "battle-london" {
+  source = "../../Modules/CloudRun/0.1.0"
+
+  ProjectName = var.ProjectName
+  GCPProjectID = var.GCPProjectID
+  GCPRegion    = "europe-west2" 
+  
+  CloudRunName = "battle-europe-west2"
+  CloudRunImage = "gcr.io/cqi-operation/sk-battle:v2.6.0C1"
+}
+
+module "battle-iowa" {
+  source = "../../Modules/CloudRun/0.1.0"
+
+  ProjectName = var.ProjectName
+  GCPProjectID = var.GCPProjectID
+  GCPRegion    = "us-central1"
+
+  CloudRunName = "battle-us-central1"
+  CloudRunImage = "gcr.io/cqi-operation/sk-battle:v2.6.0C1"
+}
