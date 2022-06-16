5edaacca0a0b4ca31977fb3fdada8cb26b680fb3
Costa Chen <developerjoy@gmail.com>
2022-06-16 09:15:57 +0800

    Fix path.

--- EnvQA/5-Apps/providers.tf
+++ EnvQA/5-Apps/providers.tf
@@ -1,24 +1,5 @@
 ﻿terraform {
-  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
-  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
-  # https://www.terraform.io/language/expressions/version-constraints
-#  required_version = "~> 1.0.11"
-
   backend "http" {}
-
-#  required_providers {
-#    // https://registry.terraform.io/providers/hashicorp/google/latest
-#    google = {
-#      source = "hashicorp/google"
-#      version = "~>4.16.0"
-#    }
-#
-#    // https://registry.terraform.io/providers/hashicorp/helm/latest
-#    helm = {
-#      source  = "hashicorp/helm"
-#      version = "~>2.5.0"
-#    }
-#  }
 }
 
 // https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
@@ -32,12 +13,12 @@ data "google_client_config" "default" {}
 
 data "google_storage_bucket_object_content" "GKE-API" {
   bucket = var.ProjectName
-  name   = "GKE.api"
+  name   = "GKE-${var.GCPRegion}.api"
 }
 
 data "google_storage_bucket_object_content" "GKE-CA" {
   bucket = var.ProjectName
-  name   = "GKE.ca"
+  name   = "GKE-${var.GCPRegion}.ca"
 }
 
 provider "helm" {
