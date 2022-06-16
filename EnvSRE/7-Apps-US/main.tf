396cb62ca357f182829b4efe7066b1ff3cbb8a40
Costa Chen <developerjoy@gmail.com>
2022-06-16 10:59:13 +0800

    Init version.

---
+++ EnvSRE/7-Apps-US/main.tf
@@ -0,0 +1,37 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "Applications" {
+  source = "../../Modules/Apps/0.2.0"
+
+  ProjectName  = var.ProjectName
+  GCPProjectID = var.GCPProjectID
+  GCPZone      = var.GCPZone
+
+  ExternalDNS_Enable          = false
+  DomainName                  = ""
+  CreateProductionCertificate = false
+
+  CertManager_Enable   = false
+  CloudSQLProxy_Enable = false
+
+  ArgoCD_Enable                       = true
+  ArgoCD_EnableSelfHeal               = false
+  ArgoCD_EnableAllApps                = false
+  ArgoCD_GitLabTokenName              = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_RepositoryHelmPathValueFiles = "{values-sre.yaml}"
+  ArgoCD_AppFileBranchOrTag           = "main"
+  ArgoCD_AppTableBranchOrTag          = "main"
+  ArgoCD_AppUserBranchOrTag           = "main"
+  ArgoCD_AppBackstageBranchOrTag      = "main"
+  ArgoCD_AppBattleBranchOrTag         = "main"
+  ArgoCD_AppNFTBranchOrTag            = "main"
+
+  AlertSlackChannel          = "alert-sk-sre"
+  PrometheusStorageClassName = "ssd-delete"
+  PrometheusStorageSize      = "20Gi"
+  PrometheusRetention        = "1d"
+  GrafanaAdminPassword       = "gra4422"
+}
+
+
+
---
+++ EnvSRE/7-Apps-US/variables.tf
@@ -0,0 +1,40 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+  default = "cqi-sk-sre"
+}
+
+variable "GCPProjectID" {
+  type    = string
+  default = "stellar-38931"
+}
+
+# https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "us-central1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "us-central1-a"
+  description = "cloud provider zone."
+}
+
+#============================
+# ArgoCD                    #
+#============================
+variable "ArgoCD_GitLabTokenName" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+variable "ArgoCD_GitLabTokenSecret" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
