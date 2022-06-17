c80c8482d7804f11a15190c3193abd854eba3c5c
Costa Chen <developerjoy@gmail.com>
2022-06-17 08:25:38 +0800

    Add GKE for london.

---
+++ EnvQA/6-GKE-EU/main.tf
@@ -0,0 +1,11 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "gke" {
+  source  = "../../Modules/GKE/0.1.0"
+
+  ProjectName = "cqi-sk-qa"
+  GCPProjectID = var.GCPProjectID
+  GCPRegion = var.GCPRegion
+  GCPZone = var.GCPZone
+  GKE-Zones = [var.GCPZone]
+}
+
---
+++ EnvQA/6-GKE-EU/variables.tf
@@ -0,0 +1,20 @@
+﻿#============================
+# Project                   #
+#============================
+variable "GCPProjectID" {
+  type    = string
+  default = "cqi-operation"
+}
+
+// https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "europe-west2"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "europe-west2-a"
+  description = "cloud provider zone."
+}
