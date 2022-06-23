0c1a0ea94a68f33eefad5d717115123d39af2f34
Costa <developerjoy@gmail.com>
2022-06-23 15:32:40 +0800

    Disable autoscaling in QA environment.

--- EnvQA/4-GKE/main.tf
+++ EnvQA/4-GKE/main.tf
@@ -7,5 +7,9 @@ module "gke" {
   GCPRegion = var.GCPRegion
   GCPZone = var.GCPZone
   GKE-Zones = [var.GCPZone]
+  
+  GKE-EnableScale-e2-standard-2 = false
+  GKE-NodeCount-e2-standard-2 = 0
+  GKE-NodeCount-e2-standard-4 = 1
 }
 
--- EnvQA/6-GKE-EU/main.tf
+++ EnvQA/6-GKE-EU/main.tf
@@ -8,7 +8,8 @@ module "gke" {
   GCPZone = var.GCPZone
   GKE-Zones = [var.GCPZone]
   
-  GKE-NodeCount-e2-standard-2 = 0
-  GKE-NodeCount-e2-standard-4 = 1
+  GKE-EnableScale-e2-standard-2 = false
+  GKE-NodeCount-e2-standard-2 = 1
+  GKE-NodeCount-e2-standard-4 = 0
 }
 
