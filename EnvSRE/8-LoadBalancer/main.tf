14200a9280863c6adca9bc24da14447fc8d091ad
Costa Chen <developerjoy@gmail.com>
2022-06-27 16:03:11 +0800

    Upgrade to 0.2.0.

--- EnvSRE/8-LoadBalancer/main.tf
+++ EnvSRE/8-LoadBalancer/main.tf
@@ -1,8 +1,9 @@
 # https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "LoadBalancer" {
-  source = "../../Modules/LoadBalancer/0.1.0"
+  source = "../../Modules/LoadBalancer/0.2.0"
 
   ProjectName  = file("../ProjectName.txt")
+  LoadBalancerDomainName = "global.origingaia.com"
 }
 
 
