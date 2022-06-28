0c1cdf2b0bb52c384ea42b27306c608c5075d2b4
Costa Chen <developerjoy@gmail.com>
2022-06-28 14:26:34 +0800

    ArgoCD upgrade to latest version.

--- Modules/Apps/0.3.0/AppArgocdFull.tf
+++ Modules/Apps/0.3.0/AppArgocdFull.tf
@@ -1,6 +1,6 @@
 ﻿# https://artifacthub.io/packages/helm/argo/argo-cd
 # https://github.com/argoproj/argo-helm/blob/main/charts/argo-cd/values.yaml
-# https://github.com/argoproj/argo-helm/blob/argo-cd-4.9.7/charts/argo-cd/values.yaml
+# https://github.com/argoproj/argo-helm/blob/argo-cd-4.9.8/charts/argo-cd/values.yaml
 # helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace
 # helm uninstall argocd -n argocd
 
@@ -13,7 +13,7 @@ resource "helm_release" "ArgoCDFull" {
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "~>4.9.7"
+  version    = "~>4.9.8"
 
   create_namespace = true
   namespace        = "argocd"
