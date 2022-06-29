7df1c637274864b5a777c2f0ecda5552a50db41d
Costa Chen <developerjoy@gmail.com>
2022-06-29 14:43:39 +0800

    Fix error.

--- EnvProdReview/3-Database/variables.tf
+++ EnvProdReview/3-Database/variables.tf
@@ -18,12 +18,3 @@ variable "GCPZone" {
   description = "cloud provider zone."
 }
 
-#============================
-# CloudSQL                  #
-#============================
-variable "CloudSQLAdminPassword" {
-  type = string
-  sensitive = true
-}
-
-
