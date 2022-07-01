2abf054187fc1213919efcbef29d986374382900
Costa Chen <developerjoy@gmail.com>
2022-07-01 09:26:08 +0800

    Add kubernetes provider.

--- EnvProdReview/8-LoadBalancer/main.tf
+++ EnvProdReview/8-LoadBalancer/main.tf
@@ -5,6 +5,14 @@ module "LoadBalancer" {
   ProjectName  = local.ProjectName
   
   LoadBalancerDomainName = "global.v2.7.0.ponponsnake.com"
+
+  ZoneTW = "asia-east1-a"
+  ZoneEU = "europe-west2-a"
+
+  providers = {
+    kubernetes.tw = kubernetes.tw
+    kubernetes.eu = kubernetes.eu
+  }
 }
 
 
--- EnvProdReview/8-LoadBalancer/providers.tf
+++ EnvProdReview/8-LoadBalancer/providers.tf
@@ -8,3 +8,40 @@ provider "google" {
   region  = var.GCPRegion    // assign default value.
   zone    = var.GCPZone      // assign default value.
 }
+
+data "google_client_config" "default" {}
+
+data "google_storage_bucket_object_content" "GKE-API-TW" {
+  bucket = local.ProjectName
+  name   = "GKE-asia-east1.api"
+}
+
+data "google_storage_bucket_object_content" "GKE-CA-TW" {
+  bucket = local.ProjectName
+  name   = "GKE-asia-east1.ca"
+}
+
+provider "kubernetes" {
+  alias = "tw"
+  host = "https://${data.google_storage_bucket_object_content.GKE-API-TW.content}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA-TW.content)
+}
+
+# permission: Storage Object Viewer
+data "google_storage_bucket_object_content" "GKE-API-EU" {
+  bucket = local.ProjectName
+  name   = "GKE-europe-west2.api"
+}
+
+data "google_storage_bucket_object_content" "GKE-CA-EU" {
+  bucket = local.ProjectName
+  name   = "GKE-europe-west2.ca"
+}
+
+provider "kubernetes" {
+  alias = "eu"
+  host = "https://${data.google_storage_bucket_object_content.GKE-API-EU.content}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA-EU.content)
+}
