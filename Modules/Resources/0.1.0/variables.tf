fadcb05eb0d7e3b26326406d514acdd0d58b0952
Costa Chen <developerjoy@gmail.com>
2022-07-01 11:59:42 +0800

    Add description and comment.

--- Modules/Resources/0.1.0/variables.tf
+++ Modules/Resources/0.1.0/variables.tf
@@ -9,7 +9,7 @@ variable "GCPProjectID" {
   type = string
 }
 
-// https://cloud.google.com/compute/docs/regions-zones
+# https://cloud.google.com/compute/docs/regions-zones
 variable "GCPRegion" {
   type        = string
   description = "cloud provider region."
