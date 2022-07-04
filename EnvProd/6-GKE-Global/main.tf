95ff793cbcbd8d8e679e57c4b0642820abc37552
Costa Chen <developerjoy@gmail.com>
2022-07-04 13:45:58 +0800

    enable scaling.

--- EnvProd/6-GKE-Global/main.tf
+++ EnvProd/6-GKE-Global/main.tf
@@ -9,7 +9,7 @@ module "GKE-US" {
   GCPZone = var.GCPZone
   GKE-Zones = [var.GCPZone]
   
-  GKE-EnableScale-e2-standard-2 = false
+  GKE-EnableScale-e2-standard-2 = true
   GKE-NodeCount-e2-standard-2 = 1
   GKE-EnableScale-e2-standard-4 = false
   GKE-NodeCount-e2-standard-4 = 0
