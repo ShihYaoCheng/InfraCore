569f1166f5de38d43b85ed326286bc63d6a20c74
Costa Chen <developerjoy@gmail.com>
2022-07-09 15:16:36 +0800

    Fix error.

--- EnvQA/7-Apps-Global/main.tf
+++ EnvQA/7-Apps-Global/main.tf
@@ -33,7 +33,6 @@ module "Apps" {
   ArgoCD_GitLabTokenName              = var.ArgoCD_GitLabTokenName
   ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
   ArgoCD_SyncWindowCronTime           = "* * * * *"
-  ArgoCD_RepositoryHelmPathValueFiles = local.HelmPathValueFiles
   ArgoCD_AppBackstageBranchOrTag      = local.AppBackstage
   ArgoCD_AppBattleBranchOrTag         = local.AppBattle
   ArgoCD_AppFileBranchOrTag           = local.AppFile
