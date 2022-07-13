df3053efc36a62024e302c831145e34dd07d51f9
Costa Chen <developerjoy@gmail.com>
2022-07-13 11:41:36 +0800

    Copy from 0.4.0.

---
+++ Modules/Apps/0.5.0/AppTempo.tf
@@ -0,0 +1,24 @@
+﻿# https://artifacthub.io/packages/helm/grafana/tempo
+# https://artifacthub.io/packages/helm/grafana/tempo-distributed
+# https://github.com/grafana/helm-charts/blob/main/charts/tempo/values.yaml
+# helm repo add grafana https://grafana.github.io/helm-charts
+# helm upgrade --install tempo grafana/tempo -n tempo --create-namespace
+# helm uninstall tempo -n tempo
+resource "helm_release" "Tempo" {
+  count = var.Tempo_Enable ? 1 : 0
+  
+  depends_on = [helm_release.Robusta]
+
+  name             = "tempo"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "tempo"
+  version          = "~>0.15.4"
+  namespace        = "tempo"
+  create_namespace = true
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+    values = [
+      templatefile("${path.module}/Values/tempo.yaml", {})
+    ]
+}
+
---
+++ Modules/Apps/0.5.0/Legacy/app-sealedsecret.tf
@@ -0,0 +1,22 @@
+﻿# https://artifacthub.io/packages/helm/bitnami-labs/sealed-secrets
+# https://github.com/bitnami-labs/sealed-secrets/tree/main/helm/sealed-secrets
+# https://github.com/bitnami-labs/sealed-secrets/blob/main/helm/sealed-secrets/values.yaml
+# https://github.com/bitnami-labs/sealed-secrets/blob/main/contrib/prometheus-mixin/dashboards/sealed-secrets-controller.json
+
+#resource "helm_release" "sealed-secret" {
+#  depends_on = [helm_release.prometheus]
+#
+#  name       = "sealed-secret"
+#  chart      = "../charts/dz-sealedsecret"
+#  namespace = "kube-system"
+#
+#  set_sensitive {
+#    name  = "keys.public"
+#    value = var.SealedSecretPublicKey
+#  }
+#
+#  set_sensitive {
+#    name  = "keys.private"
+#    value = var.SealedSecretPrivateKey
+#  }
+#}
