52935aa9f4a09d16b55aea10064f19c7816cfc20
Costa Chen <developerjoy@gmail.com>
2022-07-13 14:57:51 +0800

    The Apps upgrade to 0.5.0.

--- EnvDev/5-Apps/main.tf
+++ EnvDev/5-Apps/main.tf
@@ -1,6 +1,6 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "Apps" {
-  source = "../../Modules/Apps/0.4.0"
+  source = "../../Modules/Apps/0.5.0"
 
   ProjectName  = local.ProjectName
   GCPProjectID = local.ProjectID
@@ -16,30 +16,35 @@ module "Apps" {
 
   CloudSQLProxy_Enable = true
 
-  Prometheus_StorageClassName  = "ssd-delete"
-  Prometheus_StorageSize       = "100Gi"
-  Prometheus_Retention         = "60d"
-  Grafana_AdminPassword        = "gra4422"
+  Prometheus_StorageClassName = "ssd-delete"
+  Prometheus_StorageSize      = "100Gi"
+  Prometheus_Retention        = "60d"
+  Grafana_AdminPassword       = "gra4422"
 
   Robusta_ClusterName  = "sk-dev"
   Robusta_SlackAPIKey  = var.Robusta_SlackAPIKey
   Robusta_SlackChannel = "sk-dev-info"
 
-  ArgoCD_Enable                       = true
-  ArgoCD_EnableSelfHeal               = true
-  ArgoCD_EnableAllApps                = true
-  ArgoCD_EnableIngress                = true
-  ArgoCD_IngressUseProdCert           = true
-  ArgoCD_SyncWindowCronTime           = "* * * * *"
-  ArgoCD_GitLabTokenName              = var.ArgoCD_GitLabTokenName
-  ArgoCD_GitLabTokenSecret            = var.ArgoCD_GitLabTokenSecret
-  ArgoCD_RepositoryHelmPathValueFiles = "{values-dev.yaml}"
-  ArgoCD_AppBackstageBranchOrTag      = "main"
-  ArgoCD_AppBattleBranchOrTag         = "main"
-  ArgoCD_AppFileBranchOrTag           = "main"
-  ArgoCD_AppNFTBranchOrTag            = "main"
-  ArgoCD_AppTableBranchOrTag          = "main"
-  ArgoCD_AppUserBranchOrTag           = "main"
+  ArgoCD_Enable                  = true
+  ArgoCD_EnableSelfHeal          = true
+  ArgoCD_EnableAllApps           = true
+  ArgoCD_EnableIngress           = true
+  ArgoCD_IngressUseProdCert      = true
+  ArgoCD_SyncWindowTaipeiTime    = "* * * * *"
+  ArgoCD_GitLabTokenName         = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret       = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_AppBackstageBranchOrTag = "main"
+  ArgoCD_AppBattleBranchOrTag    = "main"
+  ArgoCD_AppFileBranchOrTag      = "main"
+  ArgoCD_AppNFTBranchOrTag       = "main"
+  ArgoCD_AppTableBranchOrTag     = "main"
+  ArgoCD_AppUserBranchOrTag      = "main"
+  ArgoCD_BackstageHelmValueFiles = "{values-dev.yaml}"
+  ArgoCD_BattleHelmValueFiles    = "{values-dev.yaml}"
+  ArgoCD_FileHelmValueFiles      = "{values-dev.yaml}"
+  ArgoCD_NFTHelmValueFiles       = "{values-dev.yaml}"
+  ArgoCD_TableHelmValueFiles     = "{values-dev.yaml}"
+  ArgoCD_UserHelmValueFiles      = "{values-dev.yaml}"
 }
 
 
