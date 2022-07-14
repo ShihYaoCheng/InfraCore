d7a0117c2819c6f0344550ddb4538a58762827c4
Costa Chen <developerjoy@gmail.com>
2022-07-14 13:40:25 +0800

    The GKE module copy from 0.1.0.

--- EnvSRE/4-GKE/main.tf
+++ EnvSRE/4-GKE/main.tf
@@ -1,6 +1,6 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "GKE-TW" {
-  source  = "../../Modules/GKE/0.1.0"
+  source  = "../../Modules/GKE/0.2.0"
 
   ProjectName = local.ProjectName
 
---
+++ Modules/GKE/0.2.0/CloudStorage.tf
@@ -0,0 +1,11 @@
+﻿resource "google_storage_bucket_object" "GKE-API" {
+  bucket  = var.ProjectName
+  name    = "GKE-${var.GCPRegion}.api"
+  content = module.gke.endpoint
+}
+
+resource "google_storage_bucket_object" "GKE-CA" {
+  bucket  = var.ProjectName
+  name    = "GKE-${var.GCPRegion}.ca"
+  content = module.gke.ca_certificate
+}
