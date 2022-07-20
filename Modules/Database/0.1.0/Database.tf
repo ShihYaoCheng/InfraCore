dc788c79e3496a09d4093aba611f013d88c413bd
Costa Chen <developerjoy@gmail.com>
2022-07-20 14:00:56 +0800

    Add new Database(UserRel) and User(cqi-user-rel).

--- Modules/Database/0.1.0/Database.tf
+++ Modules/Database/0.1.0/Database.tf
@@ -1,14 +1,21 @@
 ﻿# https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/database
 resource "mysql_database" "User" {
-  depends_on = [module.CloudSQL]
-  name = "User"
+  depends_on            = [module.CloudSQL]
+  name                  = "User"
   default_character_set = "utf8mb4"
-  default_collation = "utf8mb4_0900_ai_ci"
+  default_collation     = "utf8mb4_0900_ai_ci"
+}
+
+resource "mysql_database" "UserRel" {
+  depends_on            = [module.CloudSQL]
+  name                  = "UserRel"
+  default_character_set = "utf8mb4"
+  default_collation     = "utf8mb4_0900_ai_ci"
 }
 
 resource "mysql_database" "Backstage" {
-  depends_on = [module.CloudSQL]
-  name = "Backstage"
+  depends_on            = [module.CloudSQL]
+  name                  = "Backstage"
   default_character_set = "utf8mb4"
-  default_collation = "utf8mb4_0900_ai_ci"
+  default_collation     = "utf8mb4_0900_ai_ci"
 }
--- Modules/Database/0.1.0/Users.tf
+++ Modules/Database/0.1.0/Users.tf
@@ -1,8 +1,10 @@
 ﻿
-
+#=====================================================================================#
+#                                Backstage                                            #
+#=====================================================================================#
 # https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/user
 resource "mysql_user" "cqi-backstage" {
-  depends_on = [module.CloudSQL]
+  depends_on         = [module.CloudSQL]
   user               = "cqi-backstage"
   host               = "%" // allow any host to connected. Default: localhost(allow localhost connected only).
   plaintext_password = var.CloudSQLBackstagePassword
@@ -17,9 +19,11 @@ resource "mysql_grant" "cqi-backstage" {
   privileges = ["ALL PRIVILEGES"]
 }
 
-
+#=====================================================================================#
+#                                User                                                 #
+#=====================================================================================#
 resource "mysql_user" "cqi-user" {
-  depends_on = [module.CloudSQL]
+  depends_on         = [module.CloudSQL]
   user               = "cqi-user"
   host               = "%" // allow any host to connected. Default: localhost(allow localhost connected only).
   plaintext_password = var.CloudSQLUserPassword
@@ -30,5 +34,23 @@ resource "mysql_grant" "cqi-user" {
   user       = mysql_user.cqi-user.user
   host       = mysql_user.cqi-user.host
   database   = "User"
+  privileges = ["ALL PRIVILEGES"]
+}
+
+#=====================================================================================#
+#                                User(Release)                                        #
+#=====================================================================================#
+resource "mysql_user" "cqi-user-rel" {
+  depends_on         = [module.CloudSQL]
+  user               = "cqi-user-rel"
+  host               = "%" // allow any host to connected. Default: localhost(allow localhost connected only).
+  plaintext_password = var.CloudSQLUserPassword
+}
+
+resource "mysql_grant" "cqi-user-rel" {
+  depends_on = [module.CloudSQL]
+  user       = mysql_user.cqi-user-rel.user
+  host       = mysql_user.cqi-user-rel.host
+  database   = "UserRel"
   privileges = ["ALL PRIVILEGES"]
 }
