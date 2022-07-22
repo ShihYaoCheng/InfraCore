ff5893c8dd585572ffabc2a410b17ff7023b4aed
Costa Chen <developerjoy@gmail.com>
2022-07-22 09:55:55 +0800

    Print Load Balancer IP.

---
+++ EnvSRE/7-Apps-Global/outputs.tf
@@ -0,0 +1,11 @@
+﻿# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/service
+data "kubernetes_service" "traefik" {
+  metadata {
+    name = "traefik"
+    namespace = "traefik"
+  }
+}
+
+output "TraefikLoadBalancerIP" {
+  value = data.kubernetes_service.traefik.status
+}
