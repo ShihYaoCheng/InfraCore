f4205deebfe76d0970deae1f887269d08f1cbff5
Costa Chen <developerjoy@gmail.com>
2022-07-27 14:11:34 +0800

    Adjust GKE node pool.

--- EnvProd/4-GKE/main.tf
+++ EnvProd/4-GKE/main.tf
@@ -1,20 +1,20 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "gke" {
-  source  = "../../Modules/GKE/0.2.0"
+  source = "../../Modules/GKE/0.2.0"
 
   ProjectName = local.ProjectName
 
   GCPProjectID = local.ProjectID
-  GCPRegion = local.GCPRegion
-  GCPZone = local.GCPZone
-  GKE-Zones = [local.GCPZone]
+  GCPRegion    = local.GCPRegion
+  GCPZone      = local.GCPZone
+  GKE-Zones    = [local.GCPZone]
 
   GKE-EnableScale-e2-standard-2 = false
-  GKE-MaxCount-e2-standard-2 = 1
-  GKE-NodeCount-e2-standard-2 = 0
+  GKE-MaxCount-e2-standard-2    = 0
+  GKE-NodeCount-e2-standard-2   = 0
   GKE-EnableScale-e2-standard-4 = true
-  GKE-MaxCount-e2-standard-4 = 2
-  GKE-NodeCount-e2-standard-4 = 1
+  GKE-MaxCount-e2-standard-4    = 2
+  GKE-NodeCount-e2-standard-4   = 1
 
   GKE-APIName = local.GKE-API-TW
   GKE-CAName  = local.GKE-CA-TW
--- EnvProd/6-GKE-Global/main.tf
+++ EnvProd/6-GKE-Global/main.tf
@@ -1,20 +1,20 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "GKE-US" {
-  source  = "../../Modules/GKE/0.2.0"
+  source = "../../Modules/GKE/0.2.0"
 
   ProjectName = local.ProjectName
 
   GCPProjectID = local.ProjectID
-  GCPRegion = local.GCPRegion
-  GCPZone = local.GCPZone
-  GKE-Zones = [local.GCPZone]
-  
-  GKE-EnableScale-e2-standard-2 = true
-  GKE-MaxCount-e2-standard-2 = 2
-  GKE-NodeCount-e2-standard-2 = 1
-  GKE-EnableScale-e2-standard-4 = false
-  GKE-MaxCount-e2-standard-4 = 1
-  GKE-NodeCount-e2-standard-4 = 0
+  GCPRegion    = local.GCPRegion
+  GCPZone      = local.GCPZone
+  GKE-Zones    = [local.GCPZone]
+
+  GKE-EnableScale-e2-standard-2 = false
+  GKE-MaxCount-e2-standard-2    = 0
+  GKE-NodeCount-e2-standard-2   = 0
+  GKE-EnableScale-e2-standard-4 = true
+  GKE-MaxCount-e2-standard-4    = 2
+  GKE-NodeCount-e2-standard-4   = 1
 
   GKE-APIName = local.GKE-API-EU
   GKE-CAName  = local.GKE-CA-EU
