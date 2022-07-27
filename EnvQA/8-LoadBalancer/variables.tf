c83ce7027ee8a84cc386750cd8c697bd26199a44
Costa Chen <developerjoy@gmail.com>
2022-07-27 11:07:49 +0800

    Fix error.

--- EnvQA/8-LoadBalancer/variables.tf
+++ EnvQA/8-LoadBalancer/variables.tf
@@ -6,8 +6,8 @@ locals {
   ProjectID = local.Settings["Project"]["ID"]
   ProjectName = local.Settings["Project"]["Name"]
 
-  GKE-API-TW = local.Settings["GKE"]["TW-Dev"]["APIName"]
-  GKE-CA-TW = local.Settings["GKE"]["TW-Dev"]["CAName"]
+  GKE-API-TW = local.Settings["GKE"]["TW"]["APIName"]
+  GKE-CA-TW = local.Settings["GKE"]["TW"]["CAName"]
   GKE-API-EU = local.Settings["GKE"]["EU"]["APIName"]
   GKE-CA-EU = local.Settings["GKE"]["EU"]["CAName"]
   
