4447c6e3eba4a9382209bea1a55e873154e807f3
Costa Chen <developerjoy@gmail.com>
2022-08-16 14:36:45 +0800

    Fix error.

--- Modules/GKE-Safe/0.1.0/CloudStorage.tf
+++ Modules/GKE-Safe/0.1.0/CloudStorage.tf
@@ -1,11 +1,11 @@
-﻿#resource "google_storage_bucket_object" "GKE-API" {
-#  bucket  = var.ProjectName
-#  name    = var.GKE-APIName
-#  content = module.GKE.endpoint
-#}
-#
-#resource "google_storage_bucket_object" "GKE-CA" {
-#  bucket  = var.ProjectName
-#  name    = var.GKE-CAName
-#  content = module.GKE.ca_certificate
-#}
+﻿resource "google_storage_bucket_object" "GKE-API" {
+  bucket  = var.ProjectName
+  name    = var.GKE-APIName
+  content = module.GKE-SafeCluster.endpoint
+}
+
+resource "google_storage_bucket_object" "GKE-CA" {
+  bucket  = var.ProjectName
+  name    = var.GKE-CAName
+  content = module.GKE-SafeCluster.ca_certificate
+}
