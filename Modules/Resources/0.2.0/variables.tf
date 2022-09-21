f95936fd1745cd26d0aa9b70ca9431ba528ea910
Costa Chen <developerjoy@gmail.com>
2022-09-21 15:38:43 +0800

    Copy from 0.1.0.

---
+++ Modules/Resources/0.2.0/variables.tf
@@ -0,0 +1,16 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+}
+
+variable "GCPProjectID" {
+  type = string
+}
+
+# https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  description = "cloud provider region."
+}
