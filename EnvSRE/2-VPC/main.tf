0c8375752364b9eb360704a18bbbeddf3b18ffa1
Costa Chen <developerjoy@gmail.com>
2022-09-21 15:58:32 +0800

    Upgrade terraform to 1.2.9.

--- EnvSRE/2-VPC/main.tf
+++ EnvSRE/2-VPC/main.tf
@@ -1,6 +1,6 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/network/google/latest
 module "vpc" {
-  source  = "../../Modules/VPC/0.2.0"
+  source  = "../../Modules/VPC/0.3.0"
 
   ProjectName = local.ProjectName
 
--- Modules/VPC/0.3.0/providers.tf
+++ Modules/VPC/0.3.0/providers.tf
@@ -2,13 +2,13 @@
   # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
   # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
   # https://www.terraform.io/language/expressions/version-constraints
-  required_version = "~> 1.0.11"
+  required_version = "~> 1.2.9"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/google/latest
     google = {
       source = "hashicorp/google"
-      version = "~>4.33.0"
+      version = "~>4.37.0"
     }
   }
 }
