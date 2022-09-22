bd4caeb493147fb8e37cb38d1b7672b69de85278
Costa Chen <developerjoy@gmail.com>
2022-09-22 14:06:27 +0800

    Copy from 1.0.0.

---
+++ Modules/LoadBalancer/1.1.0/providers.tf
@@ -0,0 +1,28 @@
+﻿terraform {
+  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
+  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
+  # https://www.terraform.io/language/expressions/version-constraints
+  required_version = "~> 1.0.11"
+
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/google/latest
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.24.0"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/helm/latest
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.6.0"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/kubernetes/latest
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+
+      configuration_aliases = [ kubernetes.tw, kubernetes.eu ]
+    }
+  }
+}
