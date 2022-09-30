18f41ac1851d2546397aba81ec003743ef611e31
Costa Chen <developerjoy@gmail.com>
2022-09-30 08:48:17 +0800

    Fix error.

--- EnvDev/4-GKE/providers.tf
+++ EnvDev/4-GKE/providers.tf
@@ -8,3 +8,19 @@ provider "google" {
   region  = local.GCPRegion # assign default value.
   zone    = "asia-east1-a"   # assign default value.
 }
+
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.GKE-TW-Dev.API-Endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.GKE-TW-Dev.CA-Certificate)
+  }
+}
+
+provider "kubernetes" {
+  host = "https://${module.GKE-TW-Dev.API-Endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.GKE-TW-Dev.CA-Certificate)
+}
--- EnvDev/6-GKE-Rel/providers.tf
+++ EnvDev/6-GKE-Rel/providers.tf
@@ -8,3 +8,19 @@ provider "google" {
   region  = local.GCPRegion # assign default value.
   zone    = "asia-east1-a"   # assign default value.
 }
+
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.GKE-TW-Rel.API-Endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.GKE-TW-Rel.CA-Certificate)
+  }
+}
+
+provider "kubernetes" {
+  host = "https://${module.GKE-TW-Rel.API-Endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.GKE-TW-Rel.CA-Certificate)
+}
