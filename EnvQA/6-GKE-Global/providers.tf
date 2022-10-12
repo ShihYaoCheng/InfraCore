d946d17d5602efa73df98627b646976ef003af12
Costa Chen <developerjoy@gmail.com>
2022-10-12 15:12:59 +0800

    reformat code.

--- EnvQA/6-GKE-Global/providers.tf
+++ EnvQA/6-GKE-Global/providers.tf
@@ -4,9 +4,9 @@
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
-  project = local.ProjectID  # assign default value.
-  region  = local.GCPRegion  # assign default value.
-  zone    = local.GCPZone    # assign default value.
+  project = local.ProjectID # assign default value.
+  region  = local.GCPRegion # assign default value.
+  zone    = local.GCPZone   # assign default value.
 }
 
 data "google_client_config" "default" {}
