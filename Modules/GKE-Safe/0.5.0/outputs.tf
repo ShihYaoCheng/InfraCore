95470ea26a5adff683712e31a1975d3276233459
Costa Chen <developerjoy@gmail.com>
2022-10-13 13:35:44 +0800

    Copy from 0.4.0.

---
+++ Modules/GKE-Safe/0.5.0/outputs.tf
@@ -0,0 +1,7 @@
+﻿output "API-Endpoint" {
+  value = module.GKE-PrivateCluster.endpoint
+}
+
+output "CA-Certificate" {
+  value = module.GKE-PrivateCluster.ca_certificate
+}
---
+++ Modules/GKE-Safe/0.5.0/variables.tf
@@ -0,0 +1,93 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+}
+
+variable "GCPProjectID" {
+  type    = string
+#  default = "stellar-38931"
+}
+
+variable "UniqueName" {
+  type = string
+}
+
+# https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+#  default     = "asia-east1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+#  default     = "asia-east1-b"
+  description = "cloud provider zone."
+}
+
+#============================
+# GKE                       #
+#============================
+variable "GKE-ControlPlaneCIDR" {
+  type = string
+#  default = "10.0.0.0/28"
+}
+
+variable "GKE-Zones" {
+  type = list(string)
+#  default = ["asia-east1-a"]
+}
+
+variable "GKE-APIName" {
+  type = string
+}
+
+variable "GKE-CAName" {
+  type = string
+}
+
+variable "GKE-Labels" {
+  type = map(string)
+  default = {}
+#  default = {"name"="sre"}
+}
+
+variable "GKE-NodeSizeGB" {
+  type = number
+}
+
+variable "GKE-CheapNodePool-2C8G" {
+  type = bool
+}
+
+variable "GKE-NodePoolScale-2C8G" {
+  type = bool
+}
+
+variable "GKE-MaxNum-2C8G" {
+  type = number
+}
+
+variable "GKE-NodeNum-2C8G" {
+  type = number
+  description = "USD 56.64"
+}
+
+variable "GKE-CheapNodePool-4C16G" {
+  type = bool
+}
+
+variable "GKE-NodePoolScale-4C16G" {
+  type = bool
+}
+
+variable "GKE-MaxNum-4C16G" {
+  type = number
+}
+
+variable "GKE-NodeNum-4C16G" {
+  type = number
+  description = "USD 113.28"
+}
