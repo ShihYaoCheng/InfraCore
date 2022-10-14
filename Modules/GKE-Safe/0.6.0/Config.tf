db7070efb45e08ff025d1ec8628a031edf15c40c
Costa Chen <developerjoy@gmail.com>
2022-10-14 13:41:21 +0800

    Copy from 0.5.0.

---
+++ Modules/GKE-Safe/0.6.0/Config.tf
@@ -0,0 +1,5 @@
+﻿resource "helm_release" "Configuration" {
+  name             = "configuration"
+  chart            = "${path.module}/Charts/Configuration"
+  namespace        = "default"
+}
---
+++ Modules/GKE-Safe/0.6.0/providers.tf
@@ -0,0 +1,26 @@
+﻿terraform {
+  # https://github.com/VladRassokhin/intellij-hcl/issues/365#issuecomment-996019841
+  # https://learn.hashicorp.com/tutorials/terraform/versions#terraform-version-constraints
+  # https://www.terraform.io/language/expressions/version-constraints
+  required_version = "~> 1.2.9"
+
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/google/latest
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.37.0"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/helm/latest
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.6.0"
+    }
+
+    # https://registry.terraform.io/providers/hashicorp/kubernetes/latest
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.13.1"
+    }
+  }
+}
