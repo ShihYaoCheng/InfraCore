e9c0c50703506eaad2fbeb0c693f70693173adbe
Costa Chen <developerjoy@gmail.com>
2022-10-25 08:36:41 +0800

    The Database module copy from 0.2.0.

---
+++ Modules/Database/0.3.0/Database.tf
@@ -0,0 +1,23 @@
+﻿# https://registry.terraform.io/providers/winebarrel/mysql/latest/docs/resources/database
+resource "mysql_database" "User" {
+  depends_on            = [module.CloudSQL]
+  name                  = "User"
+  default_character_set = "utf8mb4"
+  default_collation     = "utf8mb4_0900_ai_ci"
+}
+
+resource "mysql_database" "UserRel" {
+  count = var.CloudSQLCreateReleaseUserAndDB ? 1 : 0
+
+  depends_on            = [module.CloudSQL]
+  name                  = "UserRel"
+  default_character_set = "utf8mb4"
+  default_collation     = "utf8mb4_0900_ai_ci"
+}
+
+resource "mysql_database" "Backstage" {
+  depends_on            = [module.CloudSQL]
+  name                  = "Backstage"
+  default_character_set = "utf8mb4"
+  default_collation     = "utf8mb4_0900_ai_ci"
+}
---
+++ Modules/Database/0.3.0/outputs.tf
@@ -0,0 +1,8 @@
+﻿output "PublicIP" {
+  value = "${module.CloudSQL.public_ip_address}:3306"
+}
+
+output "AdminName" {
+  value = local.AdminName
+}
+
