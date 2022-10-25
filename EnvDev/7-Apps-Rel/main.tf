2170e79ab609305214319bebd917f668a0da84c3
Costa Chen <developerjoy@gmail.com>
2022-10-25 11:35:31 +0800

    The backstage switch to the beta branch.

--- EnvDev/7-Apps-Rel/main.tf
+++ EnvDev/7-Apps-Rel/main.tf
@@ -52,7 +52,7 @@ module "Apps-Rel" {
   ArgoCD_AppUserBranchOrTag        = "main"
   ArgoCD_AppOfficialWebBranchOrTag = "main"
 
-  ArgoCD_BackstageHelmValueFiles   = "{values-main.yaml}"
+  ArgoCD_BackstageHelmValueFiles   = "{values-beta.yaml}"
   ArgoCD_BattleHelmValueFiles      = "{values-v2.11.0.yaml\\, EnableIngress.yaml}"
   ArgoCD_FileHelmValueFiles        = "{values-v2.11.0.yaml\\, EnableIngress.yaml}"
   ArgoCD_NFTHelmValueFiles         = "{values-beta.yaml}"
