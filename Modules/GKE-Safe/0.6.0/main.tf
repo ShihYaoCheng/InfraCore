6ae5d6b8817988777861cc94cd95a460a1008bfb
Costa Chen <developerjoy@gmail.com>
2022-10-27 14:08:33 +0800

    Fix error.
    The Kubernetes version 1.23.12-gke.1600 doesn't support anymore.

--- Modules/GKE-Safe/0.6.0/main.tf
+++ Modules/GKE-Safe/0.6.0/main.tf
@@ -21,7 +21,7 @@ module "GKE-PrivateCluster" {
   zones = [var.GCPZone] # worker node run on multiple zones.
   # Accepted values are `UNSPECIFIED`, `RAPID`, `REGULAR` and `STABLE`. Defaults to `REGULAR`.
   release_channel = "UNSPECIFIED"
-  kubernetes_version = "1.23.12-gke.1600"
+  kubernetes_version = "1.23.12-gke.100"
   #  kubernetes_version = "1.23.9-gke.900"
   # (Taipei) From 12:00 AM to 4:00 AM.
   maintenance_start_time = "16:00" # UTC.
