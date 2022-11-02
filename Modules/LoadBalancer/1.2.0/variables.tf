f023abd02739425098db34432014bbf2f1b6aa95
Costa Chen <developerjoy@gmail.com>
2022-11-02 14:52:45 +0800

    Copy from 1.1.0.

---
+++ Modules/LoadBalancer/1.2.0/variables.tf
@@ -0,0 +1,42 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+#  default = "cqi-sk-test"
+}
+
+variable "ZoneTW" {
+  type = string
+}
+
+variable "ZoneEU" {
+  type = string
+}
+
+#============================
+# Godaddy                   #
+#============================
+locals {
+  GodaddyFQDN = "${var.GodaddySubDomainName}.${var.GodaddyDomainName}"
+}
+
+variable "GodaddyDomainName" {
+  type = string
+  #  default = "origingaia.com"
+}
+
+variable "GodaddySubDomainName" {
+  type = string
+  #  default = "dev"
+}
+
+variable "GodaddyAPIKey" {
+  type      = string
+  sensitive = true
+}
+
+variable "GodaddyAPISecret" {
+  type      = string
+  sensitive = true
+}
