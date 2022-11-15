3e0d171a78be336d021ad5464f6699be7fad856b
Costa Chen <developerjoy@gmail.com>
2022-11-15 15:00:33 +0800

    Add CDN load balancer.

---
+++ Modules/LoadBalancer/2.0.0/Godaddy-CDN.tf
@@ -0,0 +1,30 @@
+﻿resource "helm_release" "GodaddyCDN" {
+  name             = "godaddy-cdn-load-balancer"
+  chart            = "${path.module}/Charts/Godaddy"
+  namespace        = "default"
+
+  set {
+    name  = "domainName"
+    value = var.GodaddyDomainName
+  }
+
+  set {
+    name  = "subDomainNames"
+    value = "cdn"
+  }
+
+  set {
+    name  = "ip"
+    value = google_compute_global_address.CDN.address
+  }
+
+  set_sensitive {
+    name  = "godaddy.key"
+    value = var.GodaddyAPIKey
+  }
+
+  set_sensitive {
+    name  = "godaddy.secret"
+    value = var.GodaddyAPISecret
+  }
+}
