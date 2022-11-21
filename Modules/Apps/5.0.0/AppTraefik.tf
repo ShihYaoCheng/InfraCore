54308ca707bf309f670285c328e7de715ddd7a66
Costa Chen <developerjoy@gmail.com>
2022-11-21 15:07:05 +0800

    Copy from 4.1.1.

---
+++ Modules/Apps/5.0.0/AppTraefik.tf
@@ -0,0 +1,65 @@
+﻿# https://artifacthub.io/packages/helm/traefik/traefik
+# https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
+# helm upgrade --install traefik traefik/traefik -n traefik --create-namespace
+#resource "helm_release" "Traefik" {
+#  name             = "traefik"
+#  repository       = "https://helm.traefik.io/traefik"
+#  chart            = "traefik"
+#  version          = "~>10.24.3"
+#  create_namespace = true
+#  namespace        = "traefik"
+#
+#  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+#  values = [templatefile("${path.module}/Values/traefik.yaml", {})]
+#}
+
+resource "helm_release" "TraefikResources" {
+  depends_on = [helm_release.Robusta]
+
+  name             = "traefik-resources"
+  chart            = "${path.module}/Charts/traefik-resources"
+  namespace        = "traefik"
+}
+
+data "kubernetes_service" "traefik" {
+  metadata {
+    name = "traefik"
+    namespace = "traefik"
+  }
+}
+
+resource "helm_release" "Godaddy" {
+  name             = "godaddy-traefik"
+  chart            = "${path.module}/Charts/Godaddy"
+  namespace        = "default"
+  
+  set {
+    name  = "domainName"
+    value = var.GodaddyDomainName
+  }
+
+  set {
+    name  = "enablePlainSubDomainName"
+    value = var.EnableGodaddyPlainDomain
+  }
+
+  set {
+    name  = "subDomainNames"
+    value = local.SubDomainsForGodaddyHelmValues
+  }
+  
+  set {
+    name  = "ip"
+    value = data.kubernetes_service.traefik.status.0.load_balancer.0.ingress.0.ip
+  }
+  
+  set_sensitive {
+    name  = "godaddy.key"
+    value = var.GodaddyAPIKey
+  }
+
+  set_sensitive {
+    name  = "godaddy.secret"
+    value = var.GodaddyAPISecret
+  }
+}
---
+++ Modules/Apps/5.0.0/variables.tf
@@ -0,0 +1,354 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+}
+
+#variable "UniqueName" {
+#  type = string
+#}
+
+variable "GCPProjectID" {
+  type = string
+  #  default = "stellar-38931"
+}
+
+variable "GCPZone" {
+  type        = string
+  #  default     = "asia-east1-b"
+  description = "cloud provider zone."
+}
+
+#============================
+# Godaddy                   #
+#============================
+locals {
+#  GodaddyFQDN1 = var.GodaddySubDomainName1 != "@" ? "${var.GodaddySubDomainName1}.${var.GodaddyDomainName}" : var.GodaddyDomainName
+#  GodaddyFQDN2 = var.GodaddySubDomainName2 != "@" ? "${var.GodaddySubDomainName2}.${var.GodaddyDomainName}" : var.GodaddyDomainName
+#  GodaddyFQDNs = var.GodaddySubDomainName2 != "" ? "{${local.GodaddyFQDN1}\\, ${local.GodaddyFQDN2}}" : "{${local.GodaddyFQDN1}}"
+
+  
+  # https://stackoverflow.com/questions/59381410/how-can-i-convert-a-list-to-a-string-in-terraform
+
+  # GodaddySubDomainNames = ["test1","test2","test3"]
+  # v1 = "test1,test2,test3"
+  # subDomains = "test1\\,test2\\,test3"
+  
+  # v2 = ["test1.apple.com", "test2.apple.com"]
+  # v3 = ["test1.apple.com", "test2.apple.com", "apple.com"] if EnableGodaddyPlainDomain = true
+  # v4 = "test1.apple.com, test2.apple.com, apple.com"
+  # v5 = "{test1.apple.com, test2.apple.com, apple.com}"
+  # GodaddyFQDNs = "{apple.com\\, test1.apple.com\\, test2.apple.com}"
+
+  # https://developer.hashicorp.com/terraform/language/functions/join
+  # join: convert an array to a string.
+  v1 = join(",", var.GodaddySubDomainNames)
+
+  # https://developer.hashicorp.com/terraform/language/functions/replace
+  SubDomainsForGodaddyHelmValues = replace(local.v1, ",", "\\,")
+
+  # https://developer.hashicorp.com/terraform/language/functions/formatlist
+  # produces a list of strings.
+  v2 = formatlist("%s.%s", var.GodaddySubDomainNames, var.GodaddyDomainName)
+
+  # https://developer.hashicorp.com/terraform/language/functions/concat
+  v3 = concat(local.v2, var.EnableGodaddyPlainDomain == true ? [var.GodaddyDomainName] : [])
+  
+  # https://developer.hashicorp.com/terraform/language/functions/join
+  # join: convert an array to a string.
+  v4 = join(", ", local.v3)
+
+  # https://developer.hashicorp.com/terraform/language/functions/format
+  v5 = format("{%s}", local.v4)
+
+  # https://developer.hashicorp.com/terraform/language/functions/replace
+  FQDNsForCertManager = replace(local.v5, ",", "\\,")
+}
+
+#output "FQDNs" {
+#  value = local.FQDNsForCertManager
+#}
+
+variable "GodaddyDomainName" {
+  type = string
+  #  default = "origingaia.com"
+}
+
+variable "EnableGodaddyPlainDomain" {
+  type = bool
+  description = "Plain domain = @, for example: origingaia.com"
+}
+
+variable "GodaddySubDomainNames" {
+  type        = list(string)
+  #  default = ["dev", "www", "acl"]
+  description = "@ = empty"
+}
+
+variable "GodaddyAPIKey" {
+  type      = string
+  sensitive = true
+}
+
+variable "GodaddyAPISecret" {
+  type      = string
+  sensitive = true
+}
+
+
+#============================
+# Prometheus                #
+#============================
+variable "Prometheus_StorageClassName" {
+  type = string
+  #  default = "ssd-retain"
+}
+
+variable "Prometheus_StorageSize" {
+  type = string
+  #  default = "100Gi"
+}
+
+variable "Prometheus_Retention" {
+  type = string
+  #  default = "90d"
+}
+
+variable "Grafana_AdminPassword" {
+  type = string
+}
+
+#============================
+# Robusta                   #
+#============================
+variable "Robusta_SlackChannel" {
+  type = string
+}
+
+variable "Robusta_SlackAPIKey" {
+  type      = string
+  sensitive = true
+}
+
+variable "Robusta_ClusterName" {
+  type = string
+}
+
+variable "Robusta_NotifyDeploymentEvent" {
+  type = bool
+}
+
+#============================
+# Loki                      #
+#============================
+variable "Loki_Enable" {
+  type    = bool
+  default = true
+}
+
+#============================
+# Tempo                     #
+#============================
+variable "Tempo_Enable" {
+  type    = bool
+  default = false
+}
+
+#============================
+# Velero                    #
+#============================
+variable "Velero_Enable" {
+  type    = bool
+  default = false
+}
+
+variable "Velero_BackupSchedule" {
+  type    = string
+  default = "0 1 * * *"
+}
+
+variable "Velero_BackupScheduleTTL" {
+  type        = string
+  default     = "720h"
+  description = "How long the backup file will be deleted. 720h = 30d"
+}
+
+#============================
+# Cert-Manager              #
+#============================
+variable "CertManager_Enable" {
+  type = bool
+  #  default = true
+}
+
+variable "CertManager_CreateProdCert" {
+  type = bool
+  #  default = false
+}
+
+
+#============================
+# CloudSQL Proxy            #
+#============================
+#variable "CloudSQLProxy_Enable" {
+#  type = bool
+#  #  default = true
+#}
+
+
+#============================
+# ArgoCD                    #
+#============================
+# helm upgrade --install token ./charts/tokenserver -f ./charts/values-sre.yaml -n token --create-namespace
+# helm uninstall token -n token
+# helm upgrade --install user ./charts/userserver -f ./charts/values-sre.yaml -n user --create-namespace
+# helm uninstall user -n user
+variable "ArgoCD_Enable" {
+  type = bool
+  #  default = true
+}
+
+variable "ArgoCD_EnableSelfHeal" {
+  type = bool
+  #  default = true
+}
+
+variable "ArgoCD_EnableAppBattle" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppBackstage" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppOfficialWeb" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppFile" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppNFT" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppTable" {
+  type = bool
+}
+
+variable "ArgoCD_EnableAppUser" {
+  type = bool
+}
+
+variable "ArgoCD_EnableIngress" {
+  type = bool
+  #  default = false
+}
+
+variable "ArgoCD_IngressUseProdCert" {
+  type = bool
+  #  default = false
+}
+
+variable "ArgoCD_SyncWindowTaipeiTime" {
+  type        = string
+  #  default = "* * * * *"
+  description = "Timezone: UTC + 0. Cron format: minute(0~59), hour(0~23), day of the month(1~31), month(1~12),day of the week (0~6)(Sunday to Saturday; 7 is also Sunday on some systems)"
+}
+
+variable "ArgoCD_GitLabTokenName" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+variable "ArgoCD_GitLabTokenSecret" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+# https://stackoverflow.com/questions/53846273/helm-passing-array-values-through-set
+# https://helm.sh/docs/intro/using_helm/#the-format-and-limitations-of---set
+variable "ArgoCD_BackstageSqlPassword" {
+  type      = string
+  sensitive = true
+}
+
+variable "ArgoCD_BackstageHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_OfficialWebHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_BattleHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_FileHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_NFTHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_TableHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_UserSqlPassword" {
+  type      = string
+  sensitive = true
+}
+
+variable "ArgoCD_UserHelmValueFiles" {
+  type        = string
+  description = "Sample: {values-sre.yaml}"
+}
+
+variable "ArgoCD_AppFileBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppTableBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppUserBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppBackstageBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppOfficialWebBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppBattleBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_AppNFTBranchOrTag" {
+  type = string
+}
+
+variable "ArgoCD_OfficialWebRedirectEnabled" {
+  type = bool
+}
+
+variable "ArgoCD_OfficialWebRedirectDestFQDN" {
+  type = string
+}
