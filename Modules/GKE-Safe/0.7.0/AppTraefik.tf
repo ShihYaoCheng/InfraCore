a83eb71575895a7910c74244ce5771e730c8d19e
Costa Chen <developerjoy@gmail.com>
2022-11-22 14:50:10 +0800

    Copy from 0.6.0.

---
+++ Modules/GKE-Safe/0.7.0/AppTraefik.tf
@@ -0,0 +1,14 @@
+﻿# https://artifacthub.io/packages/helm/traefik/traefik
+# https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
+# helm upgrade --install traefik traefik/traefik -n traefik --create-namespace
+resource "helm_release" "Traefik" {
+  name             = "traefik"
+  repository       = "https://helm.traefik.io/traefik"
+  chart            = "traefik"
+  version          = "~>17.0.3"
+  create_namespace = true
+  namespace        = "traefik"
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+  values = [templatefile("${path.module}/Values/Traefik.yaml", {})]
+}
