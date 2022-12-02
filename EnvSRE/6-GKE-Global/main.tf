f8f056a8d1f97595fe852ba8dbb42fc4774c79ef
Costa Chen <developerjoy@gmail.com>
2022-12-02 16:09:34 +0800

    Upgrade to 0.8.0.

--- EnvSRE/6-GKE-Global/main.tf
+++ EnvSRE/6-GKE-Global/main.tf
@@ -1,6 +1,6 @@
 ﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
 module "GKE-EU" {
-  source = "../../Modules/GKE-Safe/0.7.0"
+  source = "../../Modules/GKE-Safe/0.8.0"
 
   ProjectName = local.ProjectName
 
