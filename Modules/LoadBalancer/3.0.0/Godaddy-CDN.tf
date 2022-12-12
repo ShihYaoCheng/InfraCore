aa45b8d291093bcacd63458acedf80b284c114b4
Costa Chen <developerjoy@gmail.com>
2022-12-12 16:34:19 +0800

    Copy from 2.1.0.

---
+++ Modules/LoadBalancer/3.0.0/Godaddy-CDN.tf
@@ -0,0 +1,32 @@
+﻿resource "helm_release" "GodaddyCDN" {
+  count = var.CDNEnabled ? 1 : 0
+  
+  name             = "godaddy-cdn-load-balancer"
+  chart            = "${path.module}/Charts/Godaddy"
+  namespace        = "default"
+
+  set {
+    name  = "domainName"
+    value = var.DomainName
+  }
+
+  set {
+    name  = "subDomainNames"
+    value = local.cdnSubDomainsHelm
+  }
+
+  set {
+    name  = "ip"
+    value = google_compute_global_address.CDN[0].address
+  }
+
+  set_sensitive {
+    name  = "godaddy.key"
+    value = var.GodaddyAPIKey
+  }
+
+  set_sensitive {
+    name  = "godaddy.secret"
+    value = var.GodaddyAPISecret
+  }
+}
