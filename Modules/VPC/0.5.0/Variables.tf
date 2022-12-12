2db7cd1f821511190eacbbb45d80f407878e98a7
Costa Chen <developerjoy@gmail.com>
2022-12-12 11:28:32 +0800

    Copy from 0.4.0.

---
+++ Modules/VPC/0.5.0/Variables.tf
@@ -0,0 +1,18 @@
+﻿#============================
+# Project                   #
+#============================
+variable "ProjectName" {
+  type = string
+  description = "Unique name for every resource."
+}
+
+variable "GCPProjectID" {
+  type = string
+  description = "Set GCP Project ID."
+}
+
+variable "GCPRegion" {
+  type = string
+  description = "reference: https://cloud.google.com/compute/docs/regions-zones"
+}
+
