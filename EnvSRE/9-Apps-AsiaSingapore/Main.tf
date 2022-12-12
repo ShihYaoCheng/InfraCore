320fb962b35a7e6cbb828f55d72f42e6db1d4386
Costa Chen <developerjoy@gmail.com>
2022-12-12 15:08:13 +0800

    Add Apps for singapore.

---
+++ EnvSRE/9-Apps-AsiaSingapore/Main.tf
@@ -0,0 +1,69 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "AppsAsiaSingapore" {
+  source = "../../Modules/Apps/5.1.1"
+
+  ProjectName  = local.ProjectName
+  GCPProjectID = local.ProjectID
+  GCPZone      = var.GCPZone
+
+  GodaddyDomainName        = local.DomainName
+  GodaddySubDomainNames    = local.SubDomainNames
+  GodaddyAPIKey            = var.GodaddyAPIKey
+  GodaddyAPISecret         = var.GodaddyAPISecret
+  EnableGodaddyPlainDomain = false
+  
+  ArgoCD_OfficialWebRedirectEnabled  = false
+  ArgoCD_OfficialWebRedirectDestFQDN = ""
+  ArgoCD_OfficialWebCDNEnabled       = false
+  ArgoCD_OfficialWebCDNUrl           = ""
+
+  CertManager_Enable         = true
+  CertManager_CreateProdCert = false
+
+  Prometheus_StorageClassName = "standard"
+  Prometheus_StorageSize      = "20Gi"
+  Prometheus_Retention        = "1d"
+  Grafana_AdminPassword       = "gra4422"
+
+  Robusta_ClusterName           = "sre-singapore"
+  Robusta_SlackAPIKey           = var.Robusta_SlackAPIKey
+  Robusta_SlackChannel          = "sk-sre-info"
+  Robusta_NotifyDeploymentEvent = false
+
+  ArgoCD_Enable               = true
+  ArgoCD_EnableSelfHeal       = false
+  ArgoCD_EnableIngress        = true
+  ArgoCD_IngressUseProdCert   = false
+  ArgoCD_GitLabTokenName      = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret    = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_SyncWindowTaipeiTime = "* * * * *"
+  ArgoCD_EnableAppBackstage   = false
+  ArgoCD_EnableAppBattle      = true
+  ArgoCD_EnableAppFile        = true
+  ArgoCD_EnableAppNFT         = false
+  ArgoCD_EnableAppTable       = false
+  ArgoCD_EnableAppUser        = false
+  ArgoCD_EnableAppOfficialWeb = false
+
+  ArgoCD_AppFileBranchOrTag        = "main"
+  ArgoCD_AppTableBranchOrTag       = "main"
+  ArgoCD_AppUserBranchOrTag        = "main"
+  ArgoCD_AppBackstageBranchOrTag   = "main"
+  ArgoCD_AppBattleBranchOrTag      = "main"
+  ArgoCD_AppNFTBranchOrTag         = "main"
+  ArgoCD_AppOfficialWebBranchOrTag = "main"
+
+  ArgoCD_BackstageHelmValueFiles   = "{values-sre.yaml}"
+  ArgoCD_BattleHelmValueFiles      = "{values-sre.yaml\\, EnableNEG.yaml}"
+  ArgoCD_FileHelmValueFiles        = "{values-sre.yaml\\, EnableNEG.yaml}"
+  ArgoCD_NFTHelmValueFiles         = "{values-sre.yaml}"
+  ArgoCD_TableHelmValueFiles       = "{values-sre.yaml}"
+  ArgoCD_UserHelmValueFiles        = "{values-sre.yaml}"
+  ArgoCD_OfficialWebHelmValueFiles = "{values-sre.yaml}"
+
+  ArgoCD_BackstageSqlPassword = ""
+  ArgoCD_UserSqlPassword      = ""
+}
+
+
+
---
+++ EnvSRE/9-Apps-AsiaSingapore/Variables.tf
@@ -0,0 +1,64 @@
+﻿#============================
+# Project                   #
+#============================
+locals {
+  Settings = jsondecode(file("../Settings.json"))
+  ProjectID = local.Settings["Project"]["ID"]
+  ProjectName = local.Settings["Project"]["Name"]
+
+  GKE-API-Singapore = local.Settings["GKE"]["Singapore"]["APIName"]
+  GKE-CA-Singapore = local.Settings["GKE"]["Singapore"]["CAName"]
+
+  DomainName = local.Settings["Domain"]["Name"]
+  SubDomainNames = local.Settings["Domain"]["SubDomain"]["Singapore"]
+}
+
+# https://cloud.google.com/compute/docs/regions-zones
+variable "GCPRegion" {
+  type        = string
+  default     = "asia-southeast1"
+  description = "cloud provider region."
+}
+
+variable "GCPZone" {
+  type        = string
+  default     = "asia-southeast1-b"
+  description = "cloud provider zone."
+}
+
+#============================
+# Godaddy                   #
+#============================
+variable "GodaddyAPIKey" {
+  type      = string
+  sensitive = true
+}
+
+variable "GodaddyAPISecret" {
+  type      = string
+  sensitive = true
+}
+
+#============================
+# ArgoCD                    #
+#============================
+variable "ArgoCD_GitLabTokenName" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+variable "ArgoCD_GitLabTokenSecret" {
+  type        = string
+  sensitive   = true
+  description = "Deploy token"
+}
+
+
+#============================
+# Robusta                   #
+#============================
+variable "Robusta_SlackAPIKey" {
+  type      = string
+  sensitive = true
+}
