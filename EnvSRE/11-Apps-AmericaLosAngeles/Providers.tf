dbfa94eee8a68d898e9eaa7b8352ca0025c74e8b
Costa Chen <developerjoy@gmail.com>
2022-12-12 15:11:19 +0800

    Copy from singapore.

---
+++ EnvSRE/11-Apps-AmericaLosAngeles/Providers.tf
@@ -0,0 +1,36 @@
+﻿# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = local.ProjectID # assign default value.
+  region  = var.GCPRegion   # assign default value.
+  zone    = var.GCPZone     # assign default value.
+  credentials = file("../../../keys/dev-gitlab-sk-infra-apps.json")
+}
+
+data "google_client_config" "default" {}
+
+data "google_storage_bucket_object_content" "GKE-API" {
+  bucket = local.ProjectName
+  name   = local.GKE-API-Singapore
+}
+
+data "google_storage_bucket_object_content" "GKE-CA" {
+  bucket = local.ProjectName
+  name   = local.GKE-CA-Singapore
+}
+
+provider "helm" {
+  debug = true
+
+  kubernetes {
+    host                   = "https://${data.google_storage_bucket_object_content.GKE-API.content}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA.content)
+  }
+}
+
+provider "kubernetes" {
+  host = "https://${data.google_storage_bucket_object_content.GKE-API.content}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_storage_bucket_object_content.GKE-CA.content)
+}
+
