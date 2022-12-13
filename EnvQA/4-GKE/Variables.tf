fdcbb32552b3f2b6d495cf6f68da4cee42471f7c
Costa Chen <developerjoy@gmail.com>
2022-12-13 14:23:55 +0800

    Fix Error: Invalid index.

--- EnvQA/4-GKE/Variables.tf
+++ EnvQA/4-GKE/Variables.tf
@@ -6,10 +6,9 @@ locals {
   ProjectID = local.Settings["Project"]["ID"]
   ProjectName = local.Settings["Project"]["Name"]
 
-  GKE-API-Taiwan = local.Settings["GKE"]["TW"]["APIName"]
-  GKE-CA-Taiwan = local.Settings["GKE"]["TW"]["CAName"]
+  GKE-API-Taiwan = local.Settings["GKE"]["Taiwan"]["APIName"]
+  GKE-CA-Taiwan = local.Settings["GKE"]["Taiwan"]["CAName"]
 
-  # https://cloud.google.com/compute/docs/regions-zones
   # https://cloud.google.com/compute/docs/regions-zones
   GCPRegion = local.Settings["Project"]["Taiwan"]["Region"]
   GCPZone = local.Settings["Project"]["Taiwan"]["Zone"]
