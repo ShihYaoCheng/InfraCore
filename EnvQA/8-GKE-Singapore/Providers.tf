fd00870366366e80b61b4b44b314c4ce6eef56fb
Costa Chen <developerjoy@gmail.com>
2022-12-13 15:38:32 +0800

    Copy from london.

---
+++ EnvQA/8-GKE-Singapore/Providers.tf
@@ -0,0 +1,26 @@
+﻿terraform {
+  backend "http" {}
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = local.ProjectID # assign default value.
+  region  = local.GCPRegion # assign default value.
+  zone    = local.GCPZone   # assign default value.
+}
+
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.GKE-London.API-Endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.GKE-London.CA-Certificate)
+  }
+}
+
+provider "kubernetes" {
+  host = "https://${module.GKE-London.API-Endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.GKE-London.CA-Certificate)
+}
---
+++ EnvQA/8-GKE-Singapore/Variables.tf
@@ -0,0 +1,17 @@
+﻿#============================
+# Project                   #
+#============================
+locals {
+  Settings = jsondecode(file("../Settings.json"))
+  ProjectID = local.Settings["Project"]["ID"]
+  ProjectName = local.Settings["Project"]["Name"]
+
+  GKE-API-London = local.Settings["GKE"]["London"]["APIName"]
+  GKE-CA-London = local.Settings["GKE"]["London"]["CAName"]
+
+  # https://cloud.google.com/compute/docs/regions-zones
+  GCPRegion = local.Settings["Project"]["London"]["Region"]
+  GCPZone = local.Settings["Project"]["London"]["Zone"]
+}
+
+
