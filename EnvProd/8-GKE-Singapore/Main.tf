627a7a30953cbb6117aa8980ed8e1c064d64e5f5
Costa Chen <developerjoy@gmail.com>
2022-12-14 13:28:33 +0800

    Add singapore.

---
+++ EnvProd/8-GKE-Singapore/Main.tf
@@ -0,0 +1,34 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "GKE-Singapore" {
+  source = "../../Modules/GKE-Safe/1.0.0"
+
+  ProjectName = local.ProjectName
+
+  GCPProjectID = local.ProjectID
+  GCPRegion    = local.GCPRegion
+  GCPZone      = local.GCPZone
+
+  UniqueName = "singapore"
+
+  GKE-ControlPlaneCIDR     = "10.0.0.32/28"
+  GKE-CreateServiceAccount = true
+
+  GKE-Zones   = [local.GCPZone]
+  GKE-Labels   = { "location" = "singapore", "environment" = "production" }
+  GKE-APIName = local.GKE-API
+  GKE-CAName  = local.GKE-CA
+
+  GKE-NodeSizeGB          = 80
+  GKE-CheapNodePool-2C8G  = true
+  GKE-NodePoolScale-2C8G  = false
+  GKE-MaxNum-2C8G         = 0
+  GKE-NodeNum-2C8G        = 0
+  GKE-CheapNodePool-4C16G = true
+  GKE-NodePoolScale-4C16G = false
+  GKE-MaxNum-4C16G        = 2
+  GKE-NodeNum-4C16G       = 2
+
+  CloudSQLProxy_Enabled = false
+  CloudSQLProxy_EnableNetworkPolicy = false
+}
+
---
+++ EnvProd/9-Apps-Singapore/Main.tf
@@ -0,0 +1,72 @@
+﻿# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+module "AppsSingapore" {
+  source = "../../Modules/Apps/5.1.1"
+
+  ProjectName  = local.ProjectName
+  GCPProjectID = local.ProjectID
+  GCPZone      = local.GCPZone
+
+  GodaddyDomainName        = local.DomainName
+  GodaddySubDomainNames    = local.SubDomainNames
+  GodaddyAPIKey            = var.GodaddyAPIKey
+  GodaddyAPISecret         = var.GodaddyAPISecret
+  EnableGodaddyPlainDomain = false
+
+  ArgoCD_OfficialWebRedirectEnabled  = false
+  ArgoCD_OfficialWebRedirectDestFQDN = ""
+  ArgoCD_OfficialWebCDNEnabled       = false
+  ArgoCD_OfficialWebCDNUrl           = ""
+
+  CertManager_Enable         = true
+  CertManager_CreateProdCert = true
+
+  Prometheus_StorageClassName = "standard"
+  Prometheus_StorageSize      = "300Gi"
+  Prometheus_Retention        = "90d"
+  Grafana_AdminPassword       = var.GrafanaAdminPassword
+
+  Robusta_ClusterName           = "sk-prod-singapore"
+  Robusta_SlackAPIKey           = var.Robusta_SlackAPIKey
+  Robusta_SlackChannel          = "sk-prod-info"
+  Robusta_NotifyDeploymentEvent = true
+
+  ArgoCD_Enable               = true
+  ArgoCD_EnableSelfHeal       = true
+  ArgoCD_EnableIngress        = true
+  ArgoCD_IngressUseProdCert   = true
+  ArgoCD_GitLabTokenName      = var.ArgoCD_GitLabTokenName
+  ArgoCD_GitLabTokenSecret    = var.ArgoCD_GitLabTokenSecret
+  ArgoCD_SyncWindowTaipeiTime = "* * * * *"
+
+  ArgoCD_EnableAppBackstage   = false
+  ArgoCD_EnableAppBattle      = true
+  ArgoCD_EnableAppFile        = true
+  ArgoCD_EnableAppNFT         = false
+  ArgoCD_EnableAppTable       = false
+  ArgoCD_EnableAppUser        = false
+  ArgoCD_EnableAppOfficialWeb = false
+
+  ArgoCD_AppBackstageBranchOrTag   = ""
+  ArgoCD_AppBattleBranchOrTag      = local.AppBattle
+  ArgoCD_AppFileBranchOrTag        = local.AppFile
+  ArgoCD_AppNFTBranchOrTag         = ""
+  ArgoCD_AppTableBranchOrTag       = ""
+  ArgoCD_AppUserBranchOrTag        = ""
+  ArgoCD_AppOfficialWebBranchOrTag = ""
+
+  ArgoCD_BackstageHelmValueFiles   = "{}"
+  ArgoCD_BattleHelmValueFiles      = local.BattleHelmValueFiles
+  ArgoCD_FileHelmValueFiles        = local.FileHelmValueFiles
+  ArgoCD_NFTHelmValueFiles         = "{}"
+  ArgoCD_TableHelmValueFiles       = "{}"
+  ArgoCD_UserHelmValueFiles        = "{}"
+  ArgoCD_OfficialWebHelmValueFiles = "{}"
+
+  ArgoCD_BackstageSqlPassword = ""
+  ArgoCD_UserSqlPassword      = ""
+}
+
+output "Settings" {
+  value = local.Settings
+}
+
