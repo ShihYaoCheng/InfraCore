778572df134d7b2c25327b4c95d9202c06870b67
Costa Chen <developerjoy@gmail.com>
2022-12-14 08:06:10 +0800

    Change comment.

--- EnvSRE/1-Res/Providers.tf
+++ EnvSRE/1-Res/Providers.tf
@@ -1,14 +1,14 @@
 ﻿# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
-  project = local.ProjectID // assign default value.
-  region  = var.GCPRegion    // assign default value.
-  zone    = var.GCPZone      // assign default value.
+  project = local.ProjectID # assign default value.
+  region  = var.GCPRegion   # assign default value.
+  zone    = var.GCPZone     # assign default value.
   credentials = file("../../../keys/dev-gitlab-sk-infra-res.json")
 }
 
 provider "google-beta" {
-  project = local.ProjectID // assign default value.
-  region  = var.GCPRegion    // assign default value.
-  zone    = var.GCPZone      // assign default value.
+  project = local.ProjectID # assign default value.
+  region  = var.GCPRegion   # assign default value.
+  zone    = var.GCPZone     # assign default value.
   credentials = file("../../../keys/dev-gitlab-sk-infra-res.json")
 }
--- EnvSRE/3-Database/Providers.tf
+++ EnvSRE/3-Database/Providers.tf
@@ -1,15 +1,15 @@
 ﻿# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
-  project = local.ProjectID // assign default value.
-  region  = var.GCPRegion    // assign default value.
-  zone    = var.GCPZone      // assign default value.
+  project = local.ProjectID # assign default value.
+  region  = var.GCPRegion   # assign default value.
+  zone    = var.GCPZone     # assign default value.
   credentials = file("../../../keys/dev-gitlab-sk-infra-db.json")
 }
 
 provider "google-beta" {
-  project = local.ProjectID // assign default value.
-  region  = var.GCPRegion    // assign default value.
-  zone    = var.GCPZone      // assign default value.
+  project = local.ProjectID # assign default value.
+  region  = var.GCPRegion   # assign default value.
+  zone    = var.GCPZone     # assign default value.
   credentials = file("../../../keys/dev-gitlab-sk-infra-db.json")
 }
 
