c4f360e3699f499b9c36bd51271856d894a17c2b
Costa Chen <developerjoy@gmail.com>
2023-02-07 08:59:50 +0800

    Copy from 1.0.0.

---
+++ Modules/GKE-Safe/1.1.0/AppCloudSQLProxy.tf
@@ -0,0 +1,107 @@
+﻿locals {
+  CloudSQLNamespace = "cloud-sql"
+  
+  # Service Account format = "^[a-z](?:[-a-z0-9]{4,28}[a-z0-9])$"
+  GCP_SA_NAME = lower("${var.ProjectName}-${var.UniqueName}-sql")
+  K8S_SA_NAME = "sql-proxy"
+  
+  CloudSQLReleaseName = "cloud-sql-proxy"
+}
+
+data "google_storage_bucket_object_content" "CloudSQLInstanceName" {
+  bucket = var.ProjectName
+  name   = "CloudSQLInstanceName"
+}
+
+resource "kubernetes_namespace_v1" "CloudSQL" {
+  count = var.CloudSQLProxy_Enabled ? 1 : 0
+  
+  metadata {
+    name = local.CloudSQLNamespace
+  }
+}
+
+resource "random_id" "CloudSQL" {
+  byte_length = 2
+}
+
+# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/workload-identity
+module "CloudSQLWorkloadIdentity" {
+  count = var.CloudSQLProxy_Enabled ? 1 : 0
+  
+  depends_on = [kubernetes_namespace_v1.CloudSQL]
+
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version = "24.0.0"
+
+  project_id   = var.GCPProjectID
+  cluster_name = var.ProjectName
+  location     = var.GCPZone
+
+  name         = "cloud-sql-proxy-${random_id.CloudSQL.dec}"
+  gcp_sa_name = local.GCP_SA_NAME
+  k8s_sa_name = local.K8S_SA_NAME
+
+  namespace    = local.CloudSQLNamespace
+  roles        = ["roles/cloudsql.client"]
+}
+
+# https://artifacthub.io/packages/helm/rimusz/gcloud-sqlproxy
+# https://github.com/rimusz/charts/blob/master/stable/gcloud-sqlproxy/values.yaml
+# https://github.com/rimusz/charts/tree/master/stable/gcloud-sqlproxy/
+resource "helm_release" "CloudSQLProxy" {
+  count = var.CloudSQLProxy_Enabled ? 1 : 0
+  
+#  name             = "${local.CloudSQLReleaseName}-${random_id.CloudSQL.dec}"
+  name             = local.CloudSQLReleaseName
+  repository       = "https://charts.rimusz.net"
+  chart            = "gcloud-sqlproxy"
+  version          = "~>0.23.0"
+  namespace        = local.CloudSQLNamespace
+  
+  set {
+    name  = "cloudsql.instances[0].instance"
+    value = data.google_storage_bucket_object_content.CloudSQLInstanceName.content
+  }
+
+  set {
+    name  = "cloudsql.instances[0].project"
+    value = var.GCPProjectID
+  }
+
+  set {
+    name  = "cloudsql.instances[0].region"
+    value = var.GCPRegion
+  }
+
+  set {
+    name  = "cloudsql.instances[0].port"
+    value = "3306"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = local.K8S_SA_NAME
+  }
+  
+  set {
+#    name  = "fullnameOverride"
+    name  = "nameOverride"
+    value = local.CloudSQLReleaseName
+  }
+  
+  set {
+    name  = "networkPolicy.enabled"
+    value = tostring(var.CloudSQLProxy_EnableNetworkPolicy)
+  }
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+  values = [templatefile("${path.module}/Values/CloudSQLProxy.yaml", {})]
+}
+
+
---
+++ Modules/GKE-Safe/1.1.0/AppTraefik.tf
@@ -0,0 +1,21 @@
+﻿# https://artifacthub.io/packages/helm/traefik/traefik
+# https://github.com/traefik/traefik-helm-chart/blob/master/traefik/values.yaml
+# helm upgrade --install traefik traefik/traefik -n traefik --create-namespace
+# helm upgrade --install traefik traefik/traefik -n traefik --create-namespace --version 20.4.1 --set ingressClass.enabled=true --set ingressClass.isDefaultClass=true --set providers.kubernetesIngress.enabled=true
+resource "helm_release" "Traefik" {
+  name             = "traefik"
+  repository       = "https://helm.traefik.io/traefik"
+  chart            = "traefik"
+  version          = "~>20.4.1"
+  create_namespace = true
+  namespace        = "traefik"
+  
+  # Add the Client IP to X-Forwarded-For header.
+  set {
+    name  = "service.spec.externalTrafficPolicy"
+    value = "Local"
+  }
+
+  # https://stackoverflow.com/questions/64696721/how-do-i-pass-variables-to-a-yaml-file-in-heml-tf
+  values = [templatefile("${path.module}/Values/Traefik.yaml", {})]
+}
